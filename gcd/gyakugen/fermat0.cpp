30c48de67276aec0f678bb221307f09a6bca7ff9
happyhappyhappyhappy <ymnkkj@gmail.com>
2021-05-27 19:35:17 +0900

    2021/05/27 完了

--- gcd/gyakugen/fermat0.cpp
+++ gcd/gyakugen/fermat0.cpp
@@ -1,2 +1,30 @@
 #include<iostream>
-using namespace std; 
+// フェルマーの小定理が正しいかどうか確認する
+using std::cout; 
+using std::endl;
+// using std::pow;
+using ll = long long;
+
+ll ISPRIME=13;
+
+ll fermatTheoremCheck(ll base,ll powNum){
+    ll res = 1;
+    ll a = base;
+    while(powNum > 0){
+        if(powNum & 1){
+            res = (res * a) % ISPRIME; 
+        }        
+        a = (a * a) % ISPRIME;
+        powNum = powNum >> 1;
+    }
+    return res;
+}
+int main(int argc, char const *argv[])
+{
+    ll answer;
+    for(ll i = 2;i <= ISPRIME*2;i++){
+        cout << i << "^" << (ISPRIME-2) << "MOD " << ISPRIME << " = " \
+         << fermatTheoremCheck(i,ISPRIME-2) << endl;
+    }    
+    return 0;
+}
