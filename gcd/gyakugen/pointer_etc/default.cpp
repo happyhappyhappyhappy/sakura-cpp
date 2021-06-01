8d3c39574cc6543a40611bb1f51ceb67b253320b
happyhappyhappyhappy <ymnkkj@gmail.com>
2021-06-01 19:38:21 +0900

    2021/06/01 Fix

--- gcd/gyakugen/pointer_etc/default.cpp
+++ gcd/gyakugen/pointer_etc/default.cpp
@@ -1,5 +1,19 @@
 #include<iostream>
 using std::cout;
-void twice(int a){
-
+using std::endl;
+bool twice(int a){
+    bool retcode=true;
+    cout << a << endl;
+    a = a * 2;
+    cout << a << endl;
+    return retcode;
+}
+int main(int argc, char const *argv[])
+{
+    int b;
+    b = 10;
+    cout << "Now b=" << b << endl;
+    twice(b);
+    cout << "After b=" << b << endl;
+    return 0;
 }
