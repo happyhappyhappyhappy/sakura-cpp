d38cfb2cc332c8f84060eb40dddccbf65b480dca
happyhappyhappyhappy <ymnkkj@gmail.com>
2021-06-03 19:41:40 +0900

    実行結果追加

--- gcd/gyakugen/EuclidBase.cpp
+++ gcd/gyakugen/EuclidBase.cpp
@@ -7,10 +7,10 @@ long long modinv(long long a,long long m){
     long long u,v;
     u = 1;
     v = 0;
-    while(b > 0){
+    while(b){
         long long t;
         t = a/b;
-        a = a + t*b;
+        a = a - t*b;
         swap(a,b);
         u = u - t*v;
         swap(u,v);
@@ -27,7 +27,23 @@ int main(int argc, const char** argv) {
     for(int i = 1;i < 13;i = i +1){
         cout << i << " 's inv: " << modinv(i,13) << endl;  
     }
+    cout << "7 's inv: " << modinv(7,5) << " mod 5" << endl;
     return 0;
 }
-
+/** Result 
+under mod 13
+1 's inv: 1
+2 's inv: 7
+3 's inv: 9
+4 's inv: 10
+5 's inv: 8
+6 's inv: 11
+7 's inv: 2
+8 's inv: 5
+9 's inv: 3
+10 's inv: 4
+11 's inv: 6
+12 's inv: 12
+7 's inv: 3 mod 5
+*/  
 
