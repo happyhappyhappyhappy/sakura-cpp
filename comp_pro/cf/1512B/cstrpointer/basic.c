9fb80e5d81ad2a1b78f9c1fed99b9767bb9f536a
happyhappyhappyhappy <ymnkkj@gmail.com>
2021-06-21 19:31:39 +0900

    2021/06/21 文字列ポインタ出力のみ

---
+++ comp_pro/cf/1512B/cstrpointer/basic.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    char *a="YamanakaKohji";
+    printf("a = %s , *a = %x \n",a,*a);
+    return 0;
+}
+/**
+ result 
+ a = YamanakaKohji , *a = 59
+ **/
