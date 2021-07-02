d21a92602b2ad4adaacd9b42ea1e94c7990ba0c9
happyhappyhappyhappy <ymnkkj@gmail.com>
2021-07-02 19:32:14 +0900

    2021/07/02 色々完了

--- comp_pro/cf/1512B/cstrpointer/strfunccopy.c
+++ comp_pro/cf/1512B/cstrpointer/strfunccopy.c
@@ -5,17 +5,35 @@
 #define BUFFER 65536
 
 bool serveFunc(char* sendStr){
-
-    return true;
+    if(sendStr != NULL){
+        printf("Get String : %s\n",sendStr);
+        return true;
+    }
+    else{
+        printf("Not Get String\n");
+        return false;
+    }
 }
 
 int main(int argc, char const *argv[])
 {
     char* str;
+    int strlength;
+
     str = malloc(BUFFER);
     if(fgets(str,BUFFER,stdin)==0 || str[0]=='\n'){
-        printf("No In");
+        printf("No Input.End.\n");
     }
-    return 0;
+    strlength = strcspn(str,"\r\n");
+    printf("String Length : %d\n",strlength);
+    str[strlength] = '\0';
+    if(serveFunc(str)){
+        printf("Send String OK!\n");
+        return 0;
+    }
+    else{
+        printf("Send String Fail!\n");
+        return 1;
+    }
 }
 
--- gcd/gyakugen/pragma/onpragma.cpp
+++ gcd/gyakugen/pragma/onpragma.cpp
@@ -52,15 +52,65 @@ int main(int argc, char const *argv[])
     " x= " << x << " y= " << y << " d= " << d << "\n";
     return 0;
 }
-/*
-Main Function : invNum = 7 modNum = 5 x = 140632707166152 &x =  y = 94478354388928 &y=  d= 140725312661552
-Start extGCD a = 7 b = 5 x = 140632707166152 &x = 0x7ffd2a45a710 y = 94478354388928 &y = 0x7ffd2a45a718
-Start extGCD a = 5 b = 2 x = 94478354388928 &x = 0x7ffd2a45a718 y = 140632707166152 &y = 0x7ffd2a45a710
-Start extGCD a = 2 b = 1 x = 140632707166152 &x = 0x7ffd2a45a710 y = 94478354388928 &y = 0x7ffd2a45a718
-Start extGCD a = 1 b = 0 x = 94478354388928 &x = 0x7ffd2a45a718 y = 140632707166152 &y = 0x7ffd2a45a710
-Last extGCD x = 1 &x = 0x7ffd2a45a718 y = 0 &y = 0x7ffd2a45a710
-End extGCD d = 1 a = 2 b = 1 x = 0 &x = 0x7ffd2a45a710 y = 1 &y = 0x7ffd2a45a718
-End extGCD d = 1 a = 5 b = 2 x = 1 &x = 0x7ffd2a45a718 y = -2 &y = 0x7ffd2a45a710
-End extGCD d = 1 a = 7 b = 5 x = -2 &x = 0x7ffd2a45a710 y = 3 &y = 0x7ffd2a45a718
-Function Result : x= 3 y= 3 d= 1
-*/
+/**
+ Main Function : invNum = 317811 modNum = 832040 x= 94616911995568 &x=  y= 140724802942512 &y=  d= 0
+Start extGCD a = 317811 b = 832040 x = 94616911995568 &x = 0x7ffd0be3f528 y = 140724802942512 &y = 0x7ffd0be3f530
+Start extGCD a = 832040 b = 317811 x = 140724802942512 &x = 0x7ffd0be3f530 y = 94616911995568 &y = 0x7ffd0be3f528
+Start extGCD a = 317811 b = 196418 x = 94616911995568 &x = 0x7ffd0be3f528 y = 140724802942512 &y = 0x7ffd0be3f530
+Start extGCD a = 196418 b = 121393 x = 140724802942512 &x = 0x7ffd0be3f530 y = 94616911995568 &y = 0x7ffd0be3f528
+Start extGCD a = 121393 b = 75025 x = 94616911995568 &x = 0x7ffd0be3f528 y = 140724802942512 &y = 0x7ffd0be3f530
+Start extGCD a = 75025 b = 46368 x = 140724802942512 &x = 0x7ffd0be3f530 y = 94616911995568 &y = 0x7ffd0be3f528
+Start extGCD a = 46368 b = 28657 x = 94616911995568 &x = 0x7ffd0be3f528 y = 140724802942512 &y = 0x7ffd0be3f530
+Start extGCD a = 28657 b = 17711 x = 140724802942512 &x = 0x7ffd0be3f530 y = 94616911995568 &y = 0x7ffd0be3f528
+Start extGCD a = 17711 b = 10946 x = 94616911995568 &x = 0x7ffd0be3f528 y = 140724802942512 &y = 0x7ffd0be3f530
+Start extGCD a = 10946 b = 6765 x = 140724802942512 &x = 0x7ffd0be3f530 y = 94616911995568 &y = 0x7ffd0be3f528
+Start extGCD a = 6765 b = 4181 x = 94616911995568 &x = 0x7ffd0be3f528 y = 140724802942512 &y = 0x7ffd0be3f530
+Start extGCD a = 4181 b = 2584 x = 140724802942512 &x = 0x7ffd0be3f530 y = 94616911995568 &y = 0x7ffd0be3f528
+Start extGCD a = 2584 b = 1597 x = 94616911995568 &x = 0x7ffd0be3f528 y = 140724802942512 &y = 0x7ffd0be3f530
+Start extGCD a = 1597 b = 987 x = 140724802942512 &x = 0x7ffd0be3f530 y = 94616911995568 &y = 0x7ffd0be3f528
+Start extGCD a = 987 b = 610 x = 94616911995568 &x = 0x7ffd0be3f528 y = 140724802942512 &y = 0x7ffd0be3f530
+Start extGCD a = 610 b = 377 x = 140724802942512 &x = 0x7ffd0be3f530 y = 94616911995568 &y = 0x7ffd0be3f528
+Start extGCD a = 377 b = 233 x = 94616911995568 &x = 0x7ffd0be3f528 y = 140724802942512 &y = 0x7ffd0be3f530
+Start extGCD a = 233 b = 144 x = 140724802942512 &x = 0x7ffd0be3f530 y = 94616911995568 &y = 0x7ffd0be3f528
+Start extGCD a = 144 b = 89 x = 94616911995568 &x = 0x7ffd0be3f528 y = 140724802942512 &y = 0x7ffd0be3f530
+Start extGCD a = 89 b = 55 x = 140724802942512 &x = 0x7ffd0be3f530 y = 94616911995568 &y = 0x7ffd0be3f528
+Start extGCD a = 55 b = 34 x = 94616911995568 &x = 0x7ffd0be3f528 y = 140724802942512 &y = 0x7ffd0be3f530
+Start extGCD a = 34 b = 21 x = 140724802942512 &x = 0x7ffd0be3f530 y = 94616911995568 &y = 0x7ffd0be3f528
+Start extGCD a = 21 b = 13 x = 94616911995568 &x = 0x7ffd0be3f528 y = 140724802942512 &y = 0x7ffd0be3f530
+Start extGCD a = 13 b = 8 x = 140724802942512 &x = 0x7ffd0be3f530 y = 94616911995568 &y = 0x7ffd0be3f528
+Start extGCD a = 8 b = 5 x = 94616911995568 &x = 0x7ffd0be3f528 y = 140724802942512 &y = 0x7ffd0be3f530
+Start extGCD a = 5 b = 3 x = 140724802942512 &x = 0x7ffd0be3f530 y = 94616911995568 &y = 0x7ffd0be3f528
+Start extGCD a = 3 b = 2 x = 94616911995568 &x = 0x7ffd0be3f528 y = 140724802942512 &y = 0x7ffd0be3f530
+Start extGCD a = 2 b = 1 x = 140724802942512 &x = 0x7ffd0be3f530 y = 94616911995568 &y = 0x7ffd0be3f528
+Start extGCD a = 1 b = 0 x = 94616911995568 &x = 0x7ffd0be3f528 y = 140724802942512 &y = 0x7ffd0be3f530
+Last extGCD x = 1 &x = 0x7ffd0be3f528 y = 0 &y = 0x7ffd0be3f530
+End extGCD d = 1 a = 2 b = 1 x = 0 &x = 0x7ffd0be3f530 y = 1 &y = 0x7ffd0be3f528
+End extGCD d = 1 a = 3 b = 2 x = 1 &x = 0x7ffd0be3f528 y = -1 &y = 0x7ffd0be3f530
+End extGCD d = 1 a = 5 b = 3 x = -1 &x = 0x7ffd0be3f530 y = 2 &y = 0x7ffd0be3f528
+End extGCD d = 1 a = 8 b = 5 x = 2 &x = 0x7ffd0be3f528 y = -3 &y = 0x7ffd0be3f530
+End extGCD d = 1 a = 13 b = 8 x = -3 &x = 0x7ffd0be3f530 y = 5 &y = 0x7ffd0be3f528
+End extGCD d = 1 a = 21 b = 13 x = 5 &x = 0x7ffd0be3f528 y = -8 &y = 0x7ffd0be3f530
+End extGCD d = 1 a = 34 b = 21 x = -8 &x = 0x7ffd0be3f530 y = 13 &y = 0x7ffd0be3f528
+End extGCD d = 1 a = 55 b = 34 x = 13 &x = 0x7ffd0be3f528 y = -21 &y = 0x7ffd0be3f530
+End extGCD d = 1 a = 89 b = 55 x = -21 &x = 0x7ffd0be3f530 y = 34 &y = 0x7ffd0be3f528
+End extGCD d = 1 a = 144 b = 89 x = 34 &x = 0x7ffd0be3f528 y = -55 &y = 0x7ffd0be3f530
+End extGCD d = 1 a = 233 b = 144 x = -55 &x = 0x7ffd0be3f530 y = 89 &y = 0x7ffd0be3f528
+End extGCD d = 1 a = 377 b = 233 x = 89 &x = 0x7ffd0be3f528 y = -144 &y = 0x7ffd0be3f530
+End extGCD d = 1 a = 610 b = 377 x = -144 &x = 0x7ffd0be3f530 y = 233 &y = 0x7ffd0be3f528
+End extGCD d = 1 a = 987 b = 610 x = 233 &x = 0x7ffd0be3f528 y = -377 &y = 0x7ffd0be3f530
+End extGCD d = 1 a = 1597 b = 987 x = -377 &x = 0x7ffd0be3f530 y = 610 &y = 0x7ffd0be3f528
+End extGCD d = 1 a = 2584 b = 1597 x = 610 &x = 0x7ffd0be3f528 y = -987 &y = 0x7ffd0be3f530
+End extGCD d = 1 a = 4181 b = 2584 x = -987 &x = 0x7ffd0be3f530 y = 1597 &y = 0x7ffd0be3f528
+End extGCD d = 1 a = 6765 b = 4181 x = 1597 &x = 0x7ffd0be3f528 y = -2584 &y = 0x7ffd0be3f530
+End extGCD d = 1 a = 10946 b = 6765 x = -2584 &x = 0x7ffd0be3f530 y = 4181 &y = 0x7ffd0be3f528
+End extGCD d = 1 a = 17711 b = 10946 x = 4181 &x = 0x7ffd0be3f528 y = -6765 &y = 0x7ffd0be3f530
+End extGCD d = 1 a = 28657 b = 17711 x = -6765 &x = 0x7ffd0be3f530 y = 10946 &y = 0x7ffd0be3f528
+End extGCD d = 1 a = 46368 b = 28657 x = 10946 &x = 0x7ffd0be3f528 y = -17711 &y = 0x7ffd0be3f530
+End extGCD d = 1 a = 75025 b = 46368 x = -17711 &x = 0x7ffd0be3f530 y = 28657 &y = 0x7ffd0be3f528
+End extGCD d = 1 a = 121393 b = 75025 x = 28657 &x = 0x7ffd0be3f528 y = -46368 &y = 0x7ffd0be3f530
+End extGCD d = 1 a = 196418 b = 121393 x = -46368 &x = 0x7ffd0be3f530 y = 75025 &y = 0x7ffd0be3f528
+End extGCD d = 1 a = 317811 b = 196418 x = 75025 &x = 0x7ffd0be3f528 y = -121393 &y = 0x7ffd0be3f530
+End extGCD d = 1 a = 832040 b = 317811 x = -121393 &x = 0x7ffd0be3f530 y = 317811 &y = 0x7ffd0be3f528
+End extGCD d = 1 a = 317811 b = 832040 x = 317811 &x = 0x7ffd0be3f528 y = -121393 &y = 0x7ffd0be3f530
+Function Result : x= 317811 y= -121393 d= 1
+ **/
