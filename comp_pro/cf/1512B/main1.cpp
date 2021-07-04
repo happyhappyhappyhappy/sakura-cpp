c8a361848995d5b05291a21d2f5fe964c1d0284a
happyhappyhappyhappy <ymnkkj@gmail.com>
2021-07-04 19:09:33 +0900

    一旦コミットする

---
+++ comp_pro/cf/1512B/main1.cpp
@@ -0,0 +1,16 @@
+#ifdef DEBUG
+#define _GLIBXX_DEBUG_
+#endif
+#pragma GCC optimize("O3")
+#include<bits/stdc++.h>
+using namespace std;
+using ll=long long;
+
+int main(int argc, char const *argv[])
+{
+    // おまじない
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    return 0;
+}
