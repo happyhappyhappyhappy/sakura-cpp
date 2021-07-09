3af8b279f0cb223c4a0dc6a2d95c54f192c6c82c
happyhappyhappyhappy <ymnkkj@gmail.com>
2021-07-09 19:17:16 +0900

    2021/07/09 19:17 で作業完了

---
+++ comp_pro/cf/1536A/submodule.cpp
@@ -0,0 +1,21 @@
+#ifdef DEBUG
+#define _GLIBXX_DEBUG
+#endif
+#pragma GCC optimize("O3")
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int allproblem;
+    cin >> allproblem;
+    for (int i = 0; i < allproblem; i++)
+    {
+        
+    }
+    
+    return 0;
+}
