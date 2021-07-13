043175c81d1cf9e874bf606089f8b866c78c4de1
happyhappyhappyhappy <ymnkkj@gmail.com>
2021-07-13 10:25:40 +0900

    2021/07/13 ひとまず第一段階は完了

--- comp_pro/cf/1539B/submodule1.cpp
+++ comp_pro/cf/1539B/submodule1.cpp
@@ -8,6 +8,18 @@
 using namespace std; 
 
 bool checkProcess(string inputStr,map<char,int> &charCounter){
+    int multiple;
+    multiple = 1;
+    for (int i = 0; i < inputStr.size(); i++)
+    {
+        if(charCounter.count(inputStr.at(i))==0){
+            charCounter.insert(make_pair(inputStr.at(i),multiple));
+        }
+        else{
+            continue;
+        }
+        multiple = multiple + 1;
+    }    
     return true;
 }
 int main(int argc, char const *argv[])
@@ -19,5 +31,9 @@ int main(int argc, char const *argv[])
     cin >> inputStr;
     map<char,int> charCounter;
     checkProcess(inputStr,charCounter);
+    cout << "-----" << inputStr << "に含まれる文字とそれに対する倍数-----" << "\n";
+    for(auto itr=charCounter.begin();itr !=charCounter.end();itr++){
+        cout << itr->first << " : " << itr->second << "\n";
+    }
     return 0;
 }
