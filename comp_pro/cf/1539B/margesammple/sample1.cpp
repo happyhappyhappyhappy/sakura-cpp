7109c7beb2861d86c479db03c933e655b962afe3
happyhappyhappyhappy <ymnkkj@gmail.com>
2021-07-22 18:20:07 +0900

    commit

---
+++ comp_pro/cf/1539B/margesammple/sample1.cpp
@@ -0,0 +1,14 @@
+#ifdef DEBUG
+#define _GLIBXX_DEBUG
+#endif
+#pragma GCC optimize("O3")
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return 0;
+}
