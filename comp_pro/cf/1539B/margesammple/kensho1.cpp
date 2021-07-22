26e1f40d2fac352098bab9cbb6f7c46a11788e50
happyhappyhappyhappy <ymnkkj@gmail.com>
2021-07-22 19:46:59 +0900

    今日はここまで 2021/07/22 19:46

---
+++ comp_pro/cf/1539B/margesammple/kensho1.cpp
@@ -0,0 +1,14 @@
+#ifdef DEBUG
+#define _GLIBXX_DEBUG
+#endif
+#pragma GCC optimize("O3")
+#include<bits/stdc++.h>
+#include<string>
+// 文字列と1ケースだけ検証
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
--- comp_pro/cf/1539B/margesammple/sample1.cpp
+++ comp_pro/cf/1539B/margesammple/sample1.cpp
@@ -3,12 +3,24 @@
 #endif
 #pragma GCC optimize("O3")
 #include<bits/stdc++.h>
-
+#include<string>
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
+    int tmp,problem_line;
+    string themeString;
+    cin >> tmp >> problem_line;
+    cin >> themeString;
+    for (int i = 0; i < problem_line; i++)
+    {
+        int from_pos,to_pos;
+        cin >> from_pos >> to_pos;
+        
+        /* code */
+    }
+    
     return 0;
 }
