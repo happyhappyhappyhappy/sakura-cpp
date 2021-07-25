7b5778be460d8b2c9f97c27e8597c9de76203faa
happyhappyhappyhappy <ymnkkj@gmail.com>
2021-07-25 12:37:24 +0900

    2021/07/25 時刻 段階更新

---
+++ comp_pro/cf/1539B/main1.cpp
@@ -0,0 +1,24 @@
+#ifdef DEBUG
+#define _GLIBXX_DEBUG
+#endif
+#pragma GCC optimize("O3")
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    map<char,int> dict;
+    int tmp,problem_cnt; // 一行目から読み取る数値
+    string problem_str;
+    vector<int,int> q_pos;
+    cin >> tmp >> problem_cnt;
+    cin >> problem_str;
+    for (int i = 0; i < problem_cnt; i++)
+    {
+     //   q
+    }
+    
+    return 0;
+}
