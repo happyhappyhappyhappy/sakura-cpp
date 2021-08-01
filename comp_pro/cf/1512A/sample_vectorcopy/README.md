970d717eeb7d279ce871939b51a79a221600d0a1
happyhappyhappyhappy <ymnkkj@gmail.com>
2021-08-01 18:46:15 +0900

    一応 配列コピーの確認は完了

---
+++ comp_pro/cf/1512A/sample_vectorcopy/README.md
@@ -0,0 +1,9 @@
+# 乱数参考サイト
+
+1. [基本](https://cpprefjp.github.io/reference/random.html)
+1. [メルセンヌツイスター](https://cpprefjp.github.io/reference/random/mt19937_64.html)
+1. [範囲指定](https://cpprefjp.github.io/reference/random/uniform_int_distribution.html)
+
+-----
+
+はまったのはメルセンヌツイスター法と範囲指定を混同させたから。もしかしたら単に100で割った余りを使うだけで良かったかも。
--- comp_pro/cf/1512A/sample_vectorcopy/sample1.cpp
+++ comp_pro/cf/1512A/sample_vectorcopy/sample1.cpp
@@ -5,6 +5,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+bool copy_vector(vector<int> callNum)
+{
+    bool result = false;
+    vector<int> toCopyVector;
+    copy(callNum.begin(),callNum.end(),back_inserter(toCopyVector));
+    cout << "----Copyed Vector----\n";
+    for (int&  e:toCopyVector)
+    {
+        cout << e << " ";
+    }
+    cout << "\n";
+    
+    result = true;
+    return result;
+}
 bool member_call_input(int member_cnt,vector<int>& callNum){
     bool result=false;
     ios_base::sync_with_stdio(false);
@@ -24,6 +39,15 @@ int main(int argc, char const *argv[])
     int member_cnt;
     cin >> member_cnt;
     vector<int> member_call;
+    member_call_input(member_cnt,member_call);
+    if(copy_vector(member_call) == true){
+        cout << "OK\n";
+        return 0;
+    }
+    else{
+        cout << "NG\n";
+        return 1;
+    }
     /**   if(member_call_input(member_cnt,member_call)){
         cout << "This is input call. \n";
         for(int& e:member_call){
@@ -35,5 +59,5 @@ int main(int argc, char const *argv[])
         cout << "ERROR!! \n";
     }**/
 
-    return 0;
+    // return 0;
 }
