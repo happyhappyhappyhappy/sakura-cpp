227aae7a2a804d4a42ec0d4dce3c5fb4c3ffb60d
happyhappyhappyhappy <ymnkkj@gmail.com>
2021-08-08 19:34:16 +0900

    2021/08/08 ひとまず準備

---
+++ comp_pro/cf/1547A/defaultAnsewer/sample1.cpp
@@ -0,0 +1,12 @@
+#ifdef DEBUG
+#define _GLIBXX_DEBUG
+#endif
+#pragma GCC optimize("O3")
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
