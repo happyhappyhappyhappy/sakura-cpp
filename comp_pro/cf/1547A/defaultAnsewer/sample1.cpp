cc98c7899dc167a6c85c1129ec0736ca40e313ff
happyhappyhappyhappy <ymnkkj@gmail.com>
2021-08-09 10:22:06 +0900

    2021/08/09 ある程度まで進んできた 10:22

--- comp_pro/cf/1547A/defaultAnsewer/sample1.cpp
+++ comp_pro/cf/1547A/defaultAnsewer/sample1.cpp
@@ -6,6 +6,22 @@
 #include<cmath>
 using namespace std;
 
+bool checkNotOver(vector<int> &a,vector<int> &b,vector<int> &f){
+    bool result = true;
+    // 同じx座標にある場合
+    if(a[0] == b[0] && a[0] == f[0]){
+        cout << "同じx座標に全部並んでます\n";
+        result = false;
+        return result;
+    }
+    if(a[1] == b[1] && a[1] == f[0]){
+        cout << "同じy座標に全部並んでいます\n";
+        result = false;
+        return result;
+    }
+    return result;
+}
+
 int resolve(){
     int result = 0;
     ios_base::sync_with_stdio(false);
@@ -17,6 +33,12 @@ int resolve(){
     cin >> b[0] >> b[1];
     cin >> f[0] >> f[1];
     result = abs(a[0]-b[0])+abs(a[1]-b[1]);
+    if(checkNotOver(a,b,f)){
+        cout << "このまま使ってもOK\n" ;
+    }
+    else{
+        cout << "まだ確認することがあります\n";
+    }
     return result ;
 }
 
@@ -27,6 +49,7 @@ int main(int argc, char const *argv[])
     int testcase;
     cin >> testcase;
     for(int test=0;test<testcase;test=test+1){
+        cout << "--- テスト その " << test+1 << " \n";
         cout << resolve() << "\n";
     }
     return 0;
