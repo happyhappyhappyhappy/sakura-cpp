23d26386f5a68ff09a5f12cc23c0ba6df98e90be
happyhappyhappyhappy <ymnkkj@gmail.com>
2021-08-10 19:33:03 +0900

    2021/08/10 ひとまず作成は完了

--- comp_pro/atcoder/DP/A/onell.cpp
+++ comp_pro/atcoder/DP/A/onell.cpp
@@ -6,11 +6,14 @@
 using namespace std;
 using ll=long long;
 
-int main(){
+int main(int argc, char const *argv[])
+{
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    int point;
-    cin >> point;
-    vector<int> costs(point);
+    ll value;
+    value = 1LL << 2;
+    cout << "value of 1LL << 2\t" << value << "\n";
+    value = 1LL << 3;
+    cout << "value of 1LL << 3\t" << value << "\n";
     return 0;
 }
---
+++ comp_pro/atcoder/DP/A/submit_get1.cpp
@@ -0,0 +1,45 @@
+#ifdef DEBUG
+#define _GLIBXX_DEBUG
+#endif
+#pragma GCC optimize("O3")
+#include<bits/stdc++.h>
+using namespace std;
+using ll=long long;
+
+template<class XXX> inline bool minchange(XXX &a,XXX b){
+    bool result = false;
+    if(b < a){
+        a = b;
+        result = true;
+    }
+    return result;
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    const ll DP_TABLE=100010;
+    const ll INF_PLUS=1LL<<60;
+    ll point;
+    cin >> point;
+    vector<ll> dp(DP_TABLE);
+    vector<ll> height(DP_TABLE);
+    for(auto &e:height){
+        cin >> e;
+    }
+    for(auto &e:dp){
+        e = INF_PLUS;
+    }
+    /** for(ll j=0;j<point;j++){
+        cout << height[j] << "\t";
+    }
+    cout << "\n"; **/
+    dp.at(0) = 0;
+    for(ll j=1;j < point ;j=j+1){
+        minchange(dp[j],dp[j-1]+abs(height[j-1]-height[j]));
+        if(j > 1){
+            minchange(dp[j],dp[j-2]+abs(height[j-2]-height[j]));
+        }
+    }
+    cout << dp[point-1] << "\n";
+    return 0;
+}
