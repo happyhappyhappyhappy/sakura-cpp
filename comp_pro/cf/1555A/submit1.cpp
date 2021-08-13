386eea55d3131f469a8a3207b833376b765fafd6
happyhappyhappyhappy <ymnkkj@gmail.com>
2021-08-13 19:26:45 +0900

    一応仮回答作成

--- comp_pro/cf/1555A/submit1.cpp
+++ comp_pro/cf/1555A/submit1.cpp
@@ -6,10 +6,52 @@
 using namespace std;
 using ll=long long;
 
+ll solve(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    ll result=0;
+    ll person;
+    ll needpizza;
+    cin >> person;
+    if(person & 1){
+        needpizza = (person + 1) ;
+    }
+    {
+        needpizza = person ;
+    }
+    result = ll((needpizza * 5) / 2);
+    return result;
+}
 
 
 int main(void)
 {
-    
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    ll testsize;
+    cin >> testsize;
+    for(ll j = 0;j < testsize ; j++){
+        cout << solve() << "\n";
+    }
     return 0;
 }
+/** 実行結果
+--- in
+6
+12
+15 ← ?
+300
+1
+9999999999999999 <- ?
+3
+--- out
+30
+37 <- ?
+750
+2
+24999999999999997 <-? 
+7
+
+? は計算すると 素直に答えになる
+6枚までは単純に6枚のピザ一枚買う15になる
+ **/ 
