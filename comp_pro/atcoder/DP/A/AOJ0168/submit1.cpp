da7750fb6208c08ead05c57e401ab6d939db8f6f
happyhappyhappyhappy <ymnkkj@gmail.com>
2021-08-22 19:47:06 +0900

    今日の分完了 2021/08/22 19:47

--- comp_pro/atcoder/DP/A/AOJ0168/submit1.cpp
+++ comp_pro/atcoder/DP/A/AOJ0168/submit1.cpp
@@ -1,54 +1,47 @@
-#ifdef DEBUG
-#define _GLIBXX_DEBUG
-#endif
-#pragma GCC optimize("O3")
 #include<bits/stdc++.h>
 using namespace std;
-using ll=long long;
-static ll dptable=35;
-static ll default_comv=0;
+const long long dptable = 35;
+const long long default_comv = 0;
 
-ll solver(vector<ll>& dp,ll position){
-    ll answer = 0;
+long long solver(vector<long long>& dp,long long position){
+    long long answer = 0;
     answer = (dp[position]+3650)/3650;
     return answer;
 }
-bool dp_init(vector<ll>& dp){
+bool dp_init(vector<long long>& dp){
     bool result=true;
-    for(ll j=0;j<dptable;j++){
+    for(long long j=0;j<dptable;j++){
         dp[j]=default_comv;
     }
     return result;
 }
-bool dp_process(vector<ll>& dp){
+bool dp_process(vector<long long>& dp){
     bool result=true;
     dp[0] = 0;
     dp[1] = dp[0] + 1;
     dp[2] = dp[1] + 1;
     dp[3] = dp[2] + dp[1] + 1;
-    for(ll j=4;j<=dptable;j=j+1){
+    for(long long  j=4;j<=dptable;j=j+1){
         dp[j] = dp[j-1] + dp[j-2] + dp[j-3];
-     //   cout << j << "\t" << dp[j] << "\n";
+    //    cout << j << "\t" << dp[j] << "\n";
     }
     return result;
 }
 
 int main(void)
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    ll input_data;
-    vector<ll> dp(dptable);
+    long long  input_data;
+    vector<long long> dp(dptable);
     dp_init(dp);
     dp_process(dp);
     /**
-    for(ll j=1;j<=10;j++){
-        ll answer ;
+    for(long long  j=1;j<=10;j++){
+        long long  answer ;
         answer = solver(dp,j);
         cout << answer << "\n";
     }**/
     while(true){
-        ll answer=0;
+        long long  answer=0;
         cin >> input_data;
         if(input_data == 0){
             // 処理終了
---
+++ comp_pro/atcoder/DP/A/AOJ0168/submit2.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+const long long dptable = 31;
+const long long default_comv = 0;
+
+int main(void){
+    long long dp(dptable);
+    for(long long j=0;j<dptable;j=j+1){
+        dp[j] = default_comv;
+    }
+    dp[0] = 0;
+    dp[1] = 1;
+    dp[2] = dp[1] + 1;
+    dp[3] = dp[2] + dp[1] + 1;
+    for(long long j=4;j<dptable;j++){
+        dp[j] = dp[j-1] + dp[j-2] + dp[j-3];
+    }
+    while(true){
+        long long position;
+        long long answer;
+        cin >> position;
+        if(position == 0){
+            break;
+        }
+        answer = (dp[position]+3650) / 3650;
+        cout << answer << "\n";
+    }
+    return 0;
+}
