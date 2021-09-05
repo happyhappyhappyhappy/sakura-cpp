f90ac577b226f3211cf297eafe45e2b2c82fbce8
happyhappyhappyhappy <ymnkkj@gmail.com>
2021-09-05 18:45:26 +0900

    --- Auto-Git Commit ---

--- comp_pro/cf/1557A/inspect/toptest.cpp
+++ comp_pro/cf/1557A/inspect/toptest.cpp
@@ -11,22 +11,36 @@ void initial(void){
     cin.tie(nullptr);
     cout.tie(nullptr);
     
-#ifndef ONLINE_JUDGE
-	freopen("input.txt", "r", stdin);
-#else
-#endif
+// #ifndef ONLINE_JUDGE
+// 	freopen("input.txt", "r", stdin);
+// #else
+// #endif
 }
 
 int main(void){
     cout << fixed << setprecision(10);
     int problem_counts;
-    for (j = 0; j < problem_counts; j++)
+    cin >> problem_counts;
+    for (int j = 0; j < problem_counts; j++)
     {
         vector<double> value(5);
         for(double& e: value){
             cin >> e;
         }
-        cout << "入力結果\n";
+        sort(value.begin,value.end);
+        double top1,top2;
+        // 最後の一つだけ以外の平均＋最後の一つ
+        double top4ave,top4all;
+        top4ave = 0.0;
+        top4sum = 0.0;
+        for(int m;m<4;m=m+1){
+            top4sum = top4sum+value[m];
+        }
+        top1 = top4sum / 4.0 + value[4];
+        top3ave = 0.0;
+        top3last2 = 0.0;
+        top3sum = 0.0;
+        top3last2sum = 0.0;
     }
     
 }
