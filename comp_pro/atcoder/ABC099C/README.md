6a923c28b3b288b825b7c23e60c4e2325b7cb9b1
happyhappyhappyhappy <ymnkkj@gmail.com>
2021-09-13 18:55:36 +0900

    一応フォルダと簡単なソースを作成 2021/09/13 18:55

---
+++ comp_pro/atcoder/ABC099C/README.md
@@ -0,0 +1,14 @@
+# \[ABC099C\]\<STRANGE BANK\>
+
+## [問題のURL](https://atcoder.jp/contests/abc099/tasks/abc099_c)
+
+## [解説のURL](https://blog.hamayanhamayan.com/entry/2018/06/15/003759)
+
+## [この問題の結果](https://atcoder.jp/contests/abc099/submissions?f.Task=abc099_c&f.LanguageName=C%2B%2B&f.Status=AC&f.User=)
+
+<!---- 「問題の結果の見方」
+ PROBLEMS→問題番号一覧→回答者数→accepted＋言語をセレクトする 
+ ---->
+
+-----
+ヒント
---
+++ comp_pro/atcoder/ABC099C/submit1.cpp
@@ -0,0 +1,25 @@
+#ifdef DEBUG
+#define _GLIBXX_DEBUG
+#endif
+#pragma GCC optimize("O3")
+#include<bits/stdc++.h>
+using namespace std;
+using ll=long long;
+
+template<class XXX> inline bool changemin()
+
+void initial(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    cout << fixed << setprecision(10);    
+}
+
+int main(void){
+    initial();
+    ll DP[100010];
+    ll giveMoney;
+    cin >> giveMoney;
+
+    return 0;
+}
