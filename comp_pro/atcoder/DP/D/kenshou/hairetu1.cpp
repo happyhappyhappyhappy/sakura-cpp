a9f89c2d717011a8c9aad96335a96af7d1e799b6
happyhappyhappyhappy <ymnkkj@gmail.com>
2021-09-22 19:40:43 +0900

    microsecondの設定を頼りすぎたか 2021/09/22 19:40

--- comp_pro/atcoder/DP/D/kenshou/hairetu1.cpp
+++ comp_pro/atcoder/DP/D/kenshou/hairetu1.cpp
@@ -2,3 +2,31 @@
 #include<array>
 #include<vector>
 #include<cstdio>
+#include<chrono>
+using namespace std;
+using li = long int;
+
+int main(void)
+{
+    chrono::system_clock::time_point start,end;
+    double elaspd;
+    const li MAX=100000;
+    li a[MAX]={};
+    cout << "配列:基本型の場合\n";
+    start = chrono::system_clock::now();
+    for (li j = 0; j < MAX; j=j+1)
+    {
+        a[j] = j;
+    }
+    li sum=0;
+    for (li j = 0; j < MAX; j++)
+    {
+        sum = sum + a[j];
+    }
+    end = chrono::system_clock::now();
+    elaspd = chrono::duration_cast<chrono::microseconds>(end-start).count();
+    cout << elaspd << " μs\n" ;
+    
+    
+    return 0;
+}
---
+++ comp_pro/atcoder/DP/D/kenshou/vector1.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<array>
+#include<vector>
+#include<cstdio>
+#include<chrono>
+using namespace std;
+using namespace std::chrono;
+using li=long int;
+
+int main(void){
+    system_clock::time_point start,end;
+    double elaspd;
+    const li MAX=100000;
+    vector<li> b1(MAX);
+    cout << "Vector:配列と同じ扱い\n";
+    start = chrono::system_clock::now();
+    for(li j=0;j<MAX;j++){
+        b1[j] = j;
+    }
+    li sum=0;
+    for (li j = 0; j < MAX; j=j+1)
+    {
+        sum = sum + b1[j];
+    }
+    end = system_clock::now();
+    /*
+    TODO: chrono絡みのバグ修正からスタート
+    */
+    elaspd = duration_cast<microseconds>(end-start);
+    cout << elaspd << " μs\n";
+    return 0;   
+}
