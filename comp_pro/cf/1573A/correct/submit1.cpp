a00b5a5ca4230c5c6e127a9c42384dac534b516c
happyhappyhappyhappy <ymnkkj@gmail.com>
2021-10-17 19:24:37 +0900

    正しい答えを入れたはずだが何故か異なる 2021/10/17 19:24

---
+++ comp_pro/cf/1573A/correct/submit1.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        int result=0;
+        char a;
+        cin >> n;
+        for(int j=1;j<=n;j=j+1){
+            cin >> a;
+            result = result + (int(a)-48);
+            if(int(a) > 48 && j!= n){
+                result = result + 1;
+            }
+            cout << result << "\n";
+        }
+    }
+
+
+
+
+}
