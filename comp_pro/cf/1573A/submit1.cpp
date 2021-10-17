6abd4db0c53584211411ca39a82860d6b93ca0f8
happyhappyhappyhappy <ymnkkj@gmail.com>
2021-10-17 18:40:16 +0900

    18:40 2021/10/17

--- comp_pro/cf/1573A/submit1.cpp
+++ comp_pro/cf/1573A/submit1.cpp
@@ -4,7 +4,6 @@
 #pragma GCC optimize("O3")
 #include<bits/stdc++.h>
 using namespace std;
-using ll=long long;
 
 void initial(void){
     ios_base::sync_with_stdio(false);
@@ -13,13 +12,18 @@ void initial(void){
 //    cout << fixed << setprecision(10);    
 }
 
-ll solver(string inputNum){
-    ll result = 0;
-    ll numSize = inputNum.size();
+int solver(string inputNum){
+    int result = 0;
+    int numSize = inputNum.size();
     vector<int> eachNums(numSize);
-    for(int j=0;j < numSize;j++){
+    for(int j=numSize-1;j >= 0 ; j=j-1){
         eachNums[j]=int(inputNum[j]) - int('0');
-        cout << eachNums[j] << "\n";
+        cout << "Position : " << j << " Num:  " << eachNums[j] << "\n";
+        if(eachNums[j] != 0){
+            result = result + eachNums[j] + min(j,numSize-j-1);
+            cout << "Now Result:  " << result << "\n";
+        }
+        // cout << eachNums[j] << "\n";
     }
     return result;
 }
