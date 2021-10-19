5f19278d71c589560d0e3e13befdad47eac6c4d7
happyhappyhappyhappy <ymnkkj@gmail.com>
2021-10-19 18:25:12 +0900

    2021/10/19 18:24 素因数分解の処理を加えかけた所で終了

--- comp_pro/atcoder/tdpc/tdpcd/map/map1.cpp
+++ comp_pro/atcoder/tdpc/tdpcd/map/map1.cpp
@@ -7,6 +7,28 @@ using namespace std;
 
 int main(void){
     map<string,int> mapOfWords;
-
-
+    mapOfWords.insert(make_pair("earth",1));
+    mapOfWords.insert(make_pair("moon",2));
+    mapOfWords["sun"] = 3;
+    mapOfWords["earth"]=4;
+    map<string,int>::iterator it=mapOfWords.begin();
+    while(it != mapOfWords.end()){
+        cout<< it->first << " :: " << it->second << "\n";
+        it++;
+    }
+    
+       if(mapOfWords.insert(make_pair("earth", 1)).second == false){
+        cout << "'earth'はすでに存在しています" << "\n";
+       }
+/**
+     if(mapOfWords.insert(make_pair("earth",1)).second == false){
+        cout << "'earth'はすでに存在しています\n";
+    }**/
+    if(mapOfWords.find("sun") != mapOfWords.end()){
+        cout << "'sun'はmapOfWordに存在しています\n";
+    }
+    if(mapOfWords.find("mars") == mapOfWords.end()){
+        cout << "'mars'は存在していません\n";
+    }
+    return 0;
 }
---
+++ comp_pro/atcoder/tdpc/tdpcd/prime/prime1.cpp
@@ -0,0 +1,32 @@
+#ifdef DEBUG
+#define _GLIBXX_DEBUG
+#endif
+#pragma GCC optimize("O3")
+#include<bits/stdc++.h>
+using namespace std;
+using ll=long long;
+
+void initial(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    cout << fixed << setprecision(10);    
+}
+int N;
+ll D;
+map<ll,ll> prime_factor(ll D){
+    map<ll,ll> result;
+    for(ll j=2LL;j*j<=D;j=j+1){
+
+    }
+
+    return result;
+}
+
+int main(void){
+    initial();
+    cin >> N >> D;
+    map<ll,ll> primeMap;
+
+    return 0;
+}
