fb8327c2e446a033115ac5a7785906f29b2d24c7
happyhappyhappyhappy <ymnkkj@gmail.com>
2021-10-19 19:36:48 +0900

    2021/10/19 はここまで

--- comp_pro/atcoder/tdpc/tdpcd/prime/prime1.cpp
+++ comp_pro/atcoder/tdpc/tdpcd/prime/prime1.cpp
@@ -14,12 +14,18 @@ void initial(void){
 }
 int N;
 ll D;
-map<ll,ll> prime_factor(ll D){
+map<ll,ll> prime_factor(ll n){
+    // TODO: これのミスでやらかしたところを再度コンパイルして実行
     map<ll,ll> result;
-    for(ll j=2LL;j*j<=D;j=j+1){
-
+    for(ll j=2LL;j*j<=n;j=j+1){
+        while(n%j == 0LL){
+            ++result[j];
+            n /= j;
+        }
     }
-
+    if( n != 1LL){
+        result[n] = 1LL;
+    }
     return result;
 }
 
@@ -27,6 +33,11 @@ int main(void){
     initial();
     cin >> N >> D;
     map<ll,ll> primeMap;
-
+    primeMap = prime_factor(D);
+    map<ll,ll>::iterator it;
+    it = primeMap.begin();
+    while(it != primeMap.end()){
+        cout << it->first << " :: " << it->second << "\n";
+    }
     return 0;
 }
