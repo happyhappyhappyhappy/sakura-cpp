b00f99d2367629d573abc52b3a8f23dd2d0f61e0
happyhappyhappyhappy <ymnkkj@gmail.com>
2021-10-24 19:37:02 +0900

    マクロの検証 新しいやり方は使えなかった 2021/10/24 19:37

---
+++ comp_pro/atcoder/tdpc/tdpcd/correct/macro/sample1.cpp
@@ -0,0 +1,42 @@
+#ifdef DEBUG
+#define _GLIBXX_DEBUG
+#endif
+#pragma GCC optimize("O3")
+#include<bits/stdc++.h>
+using ll=long long;
+
+// ↓のやり方はNG
+// template<typename T>
+// constexpr FOR(T j,T n) {return for(int (j)=0;(j)<(int)(n);(j)++)}
+
+// やっぱりここであるように#defineが有効な様だ
+#define REP(i,n) for(int (j)=0;(j) < (int)(n);++(j))
+
+void initial(void){
+    using namespace std;
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+int main(void){
+    initial();
+    REP(j,10){
+        std::cout<< j << "\n";
+    }
+
+    return 0;
+}
+
+/** 出力結果 
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+**/
