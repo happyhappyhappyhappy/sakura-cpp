165d04020665e4f3d0704484f141727cc451cc1a
happyhappyhappyhappy <ymnkkj@gmail.com>
2021-10-28 19:42:27 +0900

    2021/10/28 はここまで

--- comp_pro/atcoder/tdpc/tdpcd/sample/powbiger.cpp
+++ comp_pro/atcoder/tdpc/tdpcd/sample/powbiger.cpp
@@ -3,14 +3,13 @@
 #endif
 #pragma GCC optimize("O3")
 #include<bits/stdc++.h>
-using namespace std;
 using ll=long long;
 using db=double;
 int main(void){
+    using namespace std;
 
     // db 
     /*
-    TODO: 展望
     軸->10^18
     比較->2^?,3^?,5^?
     極端なことを言えば10^18と2^xのxとなる数を求め、
@@ -18,7 +17,32 @@ int main(void){
     計算量は 64 x 64 x 64=262144
     →googleによれば54
     */
+    db tenpow=pow(10,18);
+    db twopow=1;
+    int counter;
+    counter = 1;
+    while(true){
+        twopow = twopow * 2;
+        if(tenpow < twopow){
+            cout << "2は " << counter << "で 10^18を追い越す.\n";
+            break;
+        }
+        counter = counter + 1;
+    }
+    counter = 1;
+    db threepow = 1; 
+    while(true){
+        threepow = threepow * 3;
+        if(tenpow < threepow){
+            // TODO: この文章を作成するところから作成 2021/10/28
+            cout << "2は " << counter << "で 10^18を追い越す.\n";
+            break;
+        }
+        counter = counter + 1;
+    }
+    /**
     cout << fixed << setprecision(9) << "MIN : " << DBL_MIN << 
     " MAX : " << DBL_MAX << "\n";
-    return 0;
+    **/
+   return 0;
 }
