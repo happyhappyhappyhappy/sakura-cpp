48425b4f5188ce2df8ae3e67cdc9456bbfbd400d
happyhappyhappyhappy <ymnkkj@gmail.com>
2021-10-30 19:51:31 +0900

    色々調整2021/10/30 19:51

--- comp_pro/atcoder/ABC015D/submitC1.cpp
+++ comp_pro/atcoder/ABC015D/submitC1.cpp
@@ -5,3 +5,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 using ll=long long;
+
+// TODO グローバル変数定義
+// TODO chmin,chmaxの定義
+
+void initial(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+
+int main(void){
+    initial();
+
+
+
+    return 0;
+}
