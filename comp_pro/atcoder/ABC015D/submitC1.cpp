a79f17255424d8427eddb7f9bbbaed0aa6fd1162
happyhappyhappyhappy <ymnkkj@gmail.com>
2021-10-31 18:23:51 +0900

    ある程度まで作成完了 2021/10/31 18:23

--- comp_pro/atcoder/ABC015D/submitC1.cpp
+++ comp_pro/atcoder/ABC015D/submitC1.cpp
@@ -3,23 +3,55 @@
 #endif
 #pragma GCC optimize("O3")
 #include<bits/stdc++.h>
+#define COUT(x) cout<<#x<< " = " <<(x)<< " (L" <<__LINE__<< ")" <<endl
 using namespace std;
 using ll=long long;
 
-// TODO グローバル変数定義
-// TODO chmin,chmaxの定義
+template<class XXX> inline bool changeMax(XXX& x,XXX y){
+    if(x < y){
+        x = y;
+        return true;
+    }
+    return false;
+} 
+// 大域変数DP 与えられた枚数→今貼っている枚数→今現在の幅の長さ を要素とし、価値がこの中に入る
+const int maxPhoto=55;
+const int maxusePhoto=55;
+const int maxWidth=11000;
+int DP[maxPhoto][maxusePhoto][maxWidth];
 
 void initial(void){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
+    for(int x=0;x < maxPhoto;x=x+1){
+        for(int y=0;y < maxusePhoto;y=y+1){
+            for(int z=0;z < maxWidth;z=z+1){
+                DP[maxPhoto][maxusePhoto][maxWidth]=-1;
+            }
+        }
+    }
+    DP[0][0][0]=0; // 何も貼っていなければ価値はどこにもない
 }
-
 
 int main(void){
     initial();
+    int W;
+    cin >> W;
+    int N,K;
+    cin >> N >> K;
+    vector<int> HABA(N);
+    vector<int> HIROSA(N);
+    for(int j=0;j<N;j=j+1){
+        cin >> HABA[j] >> HIROSA[j];
+    }
+    for(int j=0;j < N;j=j+1){
+        for(int aviableK=0;aviableK<=K;aviableK=aviableK+1){
+            for(int width=0;width<=W;width=width+1){
 
-
+            }
+        }
+    }
 
     return 0;
 }
