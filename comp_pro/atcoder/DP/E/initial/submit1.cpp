668842005b36611380c7a436c187237da5d674d1
happyhappyhappyhappy <ymnkkj@gmail.com>
2021-11-04 14:59:33 +0900

    2021/11/04 14:59 あと解ければ良い

--- comp_pro/atcoder/DP/E/initial/submit1.cpp
+++ comp_pro/atcoder/DP/E/initial/submit1.cpp
@@ -5,63 +5,82 @@
 #include<bits/stdc++.h>
 using namespace std;
 using ll=long long;
+
 #define COUT(x) cout<<#x<< " = " <<(x)<< " (L" <<__LINE__<< ")" <<endl
-template<class XXX> inline bool changeMin(XXX& x,XXX y){
-    if(x>y){
-        x=y;
-        return true;
-    }
-    return false;
-}
-template<class XXX> inline bool changeMax(XXX& x,XXX y){
+
+template<class XXX> bool maxChange(XXX& x,XXX y){
     if(x<y){
         x = y;
         return true;
     }
     return false;
 }
+template<class XXX> bool minChange(XXX& x,XXX y){
+    if(x>y){
+        x=y;
+        return true;
+    }
+    return false;
+}
 // 大域変数
-// TODO inf,max_n,max_v にconst を付ければエラー消える気がする
-ll INF=1LL<<60;
-int MAX_N=110;
-int MAX_V=100100;
-ll N,W;
-ll weight[110],value[1100];
-// 配列宣言する時に、単に変数を利用するのはまずい
-ll DP[MAX_N][MAX_V];
+// 配列絡みはconst定義も加えないとダメ
+
+const int MAX_N=110;
+const int MAX_VALUE=110*1000;
+
+// 配列無関係の大域変数
+ll INF=1LL<<61; // 無限の値→重さの最大値として利用
+int N; // 荷物の数
+ll W; // 利用できる重さの最大
+// 何個目の荷物か？現在の価値はいくらか？で重さが出てくるDP
+ll DP[MAX_N][MAX_VALUE];
 
 void initial(void){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
-    for(auto x=0;x<MAX_N;x++){
-        for(auto y=0;y<MAX_V;y++){
-            DP[x][y] = INF;
+    for(int j=0;j<MAX_N;j=j++){
+        for(int k=0;k<MAX_VALUE;k++){
+            DP[j][k] = INF;
         }
     }
+    // これだけ初期値。何も入れていなければ重さは無い
     DP[0][0]=0;
 }
+
 int main(void){
     initial();
     cin >> N >> W;
-    for(ll x=0;x<N;x++){
-        cin >> value[x] >> weight[x];
+    // 面倒なのでここで物と重さの配列を作って入れてしまえ
+    int value[N];
+    int weight[N];
+    for(int j=0;j<N;j++){
+        cin >> value[j] >> weight[j];
     }
-    // まずDP処理を行う
-    for(ll j=0;j<N;j++){
-        for(ll k=0;k<MAX_V;k++){
-            if( k - value[j] >= 0){
-                changeMin(DP[j+1][k],DP[j][k-value[j]]+weight[j]);
+    for(int j=0;j<=N;j++){
+        for(int k=0;k<MAX_VALUE;k++){
+            if(k-value[j]>=0){ // 2つめの要素「価格」が0にならなければこの比較を行う
+                minChange(DP[j+1][k],DP[j][k-value[j]]+weight[j]);
             }
-            changeMin(DP[j+1][k],DP[j][k]);
+            // 価格が0以下になってしまう。もしくは意図的にそのままにする
+            minChange(DP[j+1][k],DP[j][k]);
         }
     }
-    ll answer=0;
-    for(ll k=0;k<MAX_V;k++){
-        if(DP[N][k]<=W){
-            changeMax(answer,k);
+    // 全部埋め尽くしたときDPにあるデータを見てW以下を満たす＋2つめの要素が最大になるデータを求める
+    // drken型チェック
+    ll answerA=0;
+    for(int j=0;j<MAX_VALUE;j++){
+        if(DP[N][j]<=W){
+            maxChange(answerA,DP[N][j]);
         }
     }
-    cout << answer << "\n";
+    // friend型チェック
+    int answerB=MAX_VALUE-1;
+    while(DP[N][answerB]>W){
+        answerB=answerB-1;
+    }
+    cout << "drken :" << answerA << "\n";
+    cout << "friend:" << answerB << "\n";
+
     return 0;
 }
---
+++ comp_pro/atcoder/DP/E/initial/submit1_NG1.cpp
@@ -0,0 +1,67 @@
+#ifdef DEBUG
+#define _GLIBXX_DEBUG
+#endif
+#pragma GCC optimize("O3")
+#include<bits/stdc++.h>
+using namespace std;
+using ll=long long;
+#define COUT(x) cout<<#x<< " = " <<(x)<< " (L" <<__LINE__<< ")" <<endl
+template<class XXX> inline bool changeMin(XXX& x,XXX y){
+    if(x>y){
+        x=y;
+        return true;
+    }
+    return false;
+}
+template<class XXX> inline bool changeMax(XXX& x,XXX y){
+    if(x<y){
+        x = y;
+        return true;
+    }
+    return false;
+}
+// 大域変数
+// TODO inf,max_n,max_v にconst を付ければエラー消える気がする
+ll INF=1LL<<60;
+int MAX_N=110;
+int MAX_V=100100;
+ll N,W;
+ll weight[110],value[1100];
+// 配列宣言する時に、単に変数を利用するのはまずい
+ll DP[MAX_N][MAX_V];
+
+void initial(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    for(auto x=0;x<MAX_N;x++){
+        for(auto y=0;y<MAX_V;y++){
+            DP[x][y] = INF;
+        }
+    }
+    DP[0][0]=0;
+}
+int main(void){
+    initial();
+    cin >> N >> W;
+    for(ll x=0;x<N;x++){
+        cin >> value[x] >> weight[x];
+    }
+    // まずDP処理を行う
+    for(ll j=0;j<N;j++){
+        for(ll k=0;k<MAX_V;k++){
+            if( k - value[j] >= 0){
+                changeMin(DP[j+1][k],DP[j][k-value[j]]+weight[j]);
+            }
+            changeMin(DP[j+1][k],DP[j][k]);
+        }
+    }
+    ll answer=0;
+    for(ll k=0;k<MAX_V;k++){
+        if(DP[N][k]<=W){
+            changeMax(answer,k);
+        }
+    }
+    cout << answer << "\n";
+    return 0;
+}
