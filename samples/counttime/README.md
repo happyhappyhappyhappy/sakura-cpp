672470c76e4e589ab44a9fa42a0c973582b76119
happyhappyhappyhappy <ymnkkj@gmail.com>
2021-11-07 14:49:33 +0900

    形式だけ作成

---
+++ samples/counttime/README.md
@@ -0,0 +1,11 @@
+# \[ただ足し合うだけの計算時間を確認しよう\]
+
+## きっかけ
+
+`for(int j=0;j<N;j++)` では最初のDP初期化が遅かったため
+
+-----
+ヒント
+
+* 参考ページ
+    * [仕様](https://cpprefjp.github.io/reference/chrono.html)
---
+++ samples/counttime/ipp.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+#include<chrono>
+using namespace std;
+using namespace chrono;
+
+// EDPC Eを意識してDP配列の宣言
+// 配列要素の数
+const int MAX_N=110;
+const int MAX_VALUE=MAX_N*1000;
+// 各DPの宣言
