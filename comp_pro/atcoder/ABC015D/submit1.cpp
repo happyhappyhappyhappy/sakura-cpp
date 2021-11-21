838593a208280ff5c9eec19197814e720181af0a
happyhappyhappyhappy <ymnkkj@gmail.com>
2021-11-21 19:06:07 +0900

    2021/11/21 19:05 保留にします

--- comp_pro/atcoder/ABC015D/submit1.cpp
+++ comp_pro/atcoder/ABC015D/submit1.cpp
@@ -12,11 +12,16 @@ template<class XXX> inline void chmax(XXX& x,XXX y){
         x=y;
     }
 }
+template<class XXX> inline void chmin(XXX& x,XXX y){
+    if(x>y){
+        x=y;
+    }
+}
 
 // 大域固定変数
 const int MAX_WIDTH=10100; // 用紙幅
 const int MAX_SHOT=55;
-const int CAN_MAX_SHOT=55;
+const int INF=1<<29;
 // 入力値単体
 int N; // 取ったスクリーンショットの数
 int W; // 貼れる写真の幅
@@ -34,7 +39,7 @@ void initial(void){
     for(int j=0;j<MAX_SHOT;j++){
         for(int k=0;k<MAX_SHOT;j++){
             for(int m=0;m<MAX_WIDTH;m++){
-                DP[j][k][m]=-1;
+                DP[j][k][m]=INF;
             }
         }
     }
@@ -54,18 +59,18 @@ int main(void){
             for(int n_width=0;n_width<W;n_width++)
             {
               if(n_shot==K){  // 枚数問題で貼れない
-                  chmax(DP[a_shot+1][n_shot][n_width],
+                  chmin(DP[a_shot+1][n_shot][n_width],
                   DP[a_shot][n_shot][n_width]);
               }  
               else{
                   if(n_width+width[a_shot]<=W){ // 枚数も幅も問題なし
-                    chmax(
+                    chmin(
                         DP[a_shot+1][n_shot+1][n_width+width[a_shot]],
                         DP[a_shot][n_shot][n_width]+important[a_shot]
                     );
                   }
                   else{ // 幅に問題があった
-                    chmax(
+                    chmin(
                         DP[a_shot+1][n_shot][n_width],
                         DP[a_shot][n_shot][n_width]
                     );
@@ -76,10 +81,17 @@ int main(void){
             }
         }
     }
-    int answer=0;
+    int answer=0;/*
     for(int n_shot=0;n_shot<MAX_SHOT;n_shot++){
         for(int n_width=0;n_width<MAX_WIDTH;n_width++){
             chmax(answer,DP[N][n_shot][n_width]);
+        }
+    }*/
+    for(int n_shot=0;n_shot<N;n_shot++){
+        for(int n_width=0;n_width<MAX_WIDTH;n_width++){
+            if(DP[N][n_shot][n_width]<=W){
+                chmax(answer,DP[N][n_shot][n_width]);
+            }
         }
     }
     cout << answer << "\n" << flush;
