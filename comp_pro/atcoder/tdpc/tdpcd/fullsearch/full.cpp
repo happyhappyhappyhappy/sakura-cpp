306917f127e3791afed0ec55b1c473ca72347f6a
happyhappyhappyhappy <ymnkkj@gmail.com>
2021-11-28 18:51:52 +0900

    試し作成

---
+++ comp_pro/atcoder/tdpc/tdpcd/fullsearch/full.cpp
@@ -0,0 +1,28 @@
+#ifdef DEBUG
+#define _GLIBXX_DEBUG
+#endif
+#pragma GCC optimize("O3")
+#include<bits/stdc++.h>
+using namespace std;
+using ll=long long;
+#define COUT(x) cout<<#x<< " = " <<(x)<< " (L" <<__LINE__<< ")" <<"\n" << flush
+
+void initial(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    cout << fixed << setprecision(10);
+}
+
+int main(void){
+    initial()
+    ll N,D;
+    cin >> N >> D;
+    ll counter=0;
+    for(ll f=1;f<=6;f++){
+        for(ll s=1;s<=6;s++){
+            // TODO さらに内部
+        }
+    }
+    return 0;
+}
