abc8155e35dce4388132a5abdf6324f00f06556e
happyhappyhappyhappy <ymnkkj@gmail.com>
2021-12-02 15:14:01 +0900

    2021/12/02 15:14

---
+++ samples/move/sample2.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#include<chrono>
+#include<algorithm>
+using namespace std;
+using namespace std::chrono;
+// cf https://cpprefjp.github.io/reference/chrono.html
+using ll=long long;
+#define COUT(x) cout<<#x<< " = " <<(x)<< " (L" <<__LINE__<< ")" <<"\n" << flush
+// TODO: 注釈付ける
+int main(void){
+
+    return 0;
+}
