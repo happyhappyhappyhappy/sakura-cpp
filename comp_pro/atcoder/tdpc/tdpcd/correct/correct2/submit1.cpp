2435fdce57c677bc17781223464e1b6d8e48bc2e
happyhappyhappyhappy <ymnkkj@gmail.com>
2021-12-07 19:39:18 +0900

    一応データを処理するところまでやった

--- comp_pro/atcoder/tdpc/tdpcd/correct/correct2/submit1.cpp
+++ comp_pro/atcoder/tdpc/tdpcd/correct/correct2/submit1.cpp
@@ -14,8 +14,20 @@ int main(void){
     initial();
     ll N,D;
     cin >> N >> D; // N => サイコロを振る回数,D => 振った結果この倍数か
-    int pf[3];
+    int pf[3]={0,0,0};
     int div[3] = {2,3,5};
-    
+    for(int j=0;j<3;j=j+1){
+        while(D % div[j] == 0){
+            pf[j]=pf[j]+1;
+            D = D / div[j];
+        }
+    }
+    if( D != 1 ){
+        cout << 0.0 << "\n" << fflush;
+    }
+    else{
+        cout << pf[0] << ":" << pf[1] 
+        << ":" << pf[2] << "\n" << fflush; 
+    }
     return 0;
 }
