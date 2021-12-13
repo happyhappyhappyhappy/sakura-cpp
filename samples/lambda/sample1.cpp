6207560b3e2678e06ac10493994dd2d9823e8c5e
happyhappyhappyhappy <ymnkkj@gmail.com>
2021-12-13 17:15:50 +0900

    lambda 復活

---
+++ samples/lambda/sample1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+class Mul{
+    private:
+        int j_;
+    public:
+        Mul(int i):j_(i){}
+        int operator()(int k){return k*j_;}
+};
+
+int main(void){
+    auto doubler = Mul(2);
+    cout << "doubler(10)\t" << 
+    doubler(10) << "\n" << flush;
+    cout << "doubler(123)\t" << 
+    doubler(123) << "\n" << flush;
+}
