20cc9e72f4b0b474dbc50ac1f00a2ef0436878d7
happyhappyhappyhappy <ymnkkj@gmail.com>
2021-12-20 19:31:19 +0900

    ここまで

---
+++ comp_pro/atcoder/tdpc/tdpcd/coin/sample1.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll=long long;
+#define COUT(x) cout<<#x<< " = " <<(x)<< " (L" <<__LINE__<< ")" <<"\n" << flush
+
+void initial(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    
+}
+
+int main(void){
+    initial();
+    return 0;
+}
