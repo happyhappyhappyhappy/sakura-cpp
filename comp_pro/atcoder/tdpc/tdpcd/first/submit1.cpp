5bbe3e26b612a0e9be52eaf0666becf10b9f3335
happyhappyhappyhappy <ymnkkj@gmail.com>
2021-12-24 19:38:29 +0900

    sample

---
+++ comp_pro/atcoder/tdpc/tdpcd/first/submit1.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll=long long;
+#define COUT(x) cout<<#x<< " = " <<(x)<< " (L" <<__LINE__<< ")" <<"\n" << flush
+// 大域変数 TODO : DP絡みは特に設定
+
+void initial(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    cout << fixed << setprecision(10);
+    // TODO DPの初期設定
+}
+
+int main(void){
+    initial();
+    return 0;
+}
