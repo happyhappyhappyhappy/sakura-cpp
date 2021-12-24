0c858357f49000f7d9aab53ead0aec87a80e4fea
happyhappyhappyhappy <ymnkkj@gmail.com>
2021-12-24 17:17:55 +0900

    2021/12/24 コイン問題関数そのものは完了

--- comp_pro/atcoder/tdpc/tdpcd/coin/dp1.cpp
+++ comp_pro/atcoder/tdpc/tdpcd/coin/dp1.cpp
@@ -5,16 +5,14 @@ using ll=long long;
 // 2021/12/24 コイン問題dpの単純なアプローチで解いた物
 
 // 大域変数
-const int repel = 4;
-const int summax = 8;
-double dp[repel+1][summax+1];
+double dp[5][9];
 void initial(void){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
     cout << fixed << setprecision(10);
-    for(int ii=0;ii <= repel;++ii){
-        for(int j=0;j<=summax ; ++j){
+    for(int ii=0;ii < 5;++ii){
+        for(int j=0;j < 9 ; ++j){
             dp[ii][j]=0.0;
         }
     }
@@ -23,6 +21,24 @@ void initial(void){
 
 int main(void){
     initial();
-    // 動的計画法の処理 
+    for(int ii = 0;ii < 4;ii++){
+        for(int j=0;j < 9;j++){
+            // if(dp[ii][j] > 0.0){
+            dp[ii+1][j+1] = dp[ii+1][j+1]+dp[ii][j]/2.0;
+            dp[ii+1][j+2] = dp[ii+1][j+2]+dp[ii][j]/2.0;
+            // }
+        }
+    }
+    double answer = 0.0;
+    for(int ii=6;ii<=8;ii++){
+        answer = answer + dp[4][ii];
+    }
+    cout << answer << "\n" << flush;
+    for(int ii=0;ii < 5;ii++){
+        for(int j=0; j < 9;j++){
+            cout << dp[ii][j] << " ";
+        }
+        cout << "\n" << flush;
+    }
     return 0;
 }
