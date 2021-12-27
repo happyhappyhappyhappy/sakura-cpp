ccb3b2f1924130cd0a89511f271ec915199ac86e
happyhappyhappyhappy <ymnkkj@gmail.com>
2021-12-27 19:32:57 +0900

    素因数分解箇所の作成まで完了 2021/12/27 19:32

--- comp_pro/atcoder/tdpc/tdpcd/first/submit1.cpp
+++ comp_pro/atcoder/tdpc/tdpcd/first/submit1.cpp
@@ -12,54 +12,77 @@ static int Nmax = 100;
 static int X2max = 60;
 static int X3max = 38;
 static int X5max = 26;
-double dp[Nmax+1][X2max+1][X3max+1][X5max+1];
+// ↓static としても変数として配列宣言は出来ない
+double dp[100+1][60+1][38+1][26+1];
 
 void initial(void){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
     cout << fixed << setprecision(10);
-    for(ll ii=0;ii <= Nmax ;ii++){
-        for(ll j=0;j<= X2max;j++){
-            for(ll k=0;k<= X3max;k++){
-                for(ll m=0;m<= X5max;m++){
+    for(int ii=0;ii<=Nmax;ii++){
+        for(int j=0;j<=X2max;j++){
+            for(int k=0;k<=X3max;k++){
+                for(int m=0;m<=X5max;m++){
                     dp[ii][j][k][m]=0.0;
                 }
             }
         }
     }
-    dp[0][0][0][0]=1.0000;
+    dp[0][0][0][0]=1.0;
 }
-/**
-bool prime_fac(ll &x2,ll &x3,ll &x5,&d){
-    // 与えられた数にはどれくらい2,3,5の倍数があるか
-    // 引数 x2(2の倍数),x3(3の倍数),x5(5の倍数),与えられた数D
-    // 出力 bool : true Dの残骸が1である(つまりサイコロの積に含む)
-    //          : false 1以外(この値だとどうしてもサイコロの積にならない)
-    // それ以外の情報は、引数は参照渡しにする
+
+bool prime_fac(int &x2,int &x3,int &x5,ll &d){
     // 2の倍数いくつになるか
     while(true){
-        if(d % 2)// TODO: 2の累乗数をいくつかにするかからスタート
+        if(d % 2LL != 0){
+            break;
+        }
+        x2 = x2+1;
+        d = d / 2LL;
+    }
+    // 3の倍数いくつになるか
+    while(true){
+        if(d % 3LL != 0){
+            break;
+        }
+        x3 = x3 + 1;
+        d = d / 3LL;
+    }
+    while(true){
+        if(d % 5LL != 0){
+            break;
+        }
+        x5 = x5 + 1;
+        d = d / 5LL;
+    }
+    if(d != 1){ // 2,3,5で割り切れなかった分
+        return false;
     }
     return true;
 }
-*/
+
 int main(void){
     initial();
     ll N,D;
     cin >> N >> D;// N:振る数 D:倍数
-    ll X2,X3,X5,surpus_D;
+    int X2,X3,X5;
+    ll surpus_D;
+    bool can_xi;
     X2 = 0;
     X3 = 0;
     X5 = 0;
     surpus_D = D;
-    bool exist_ed=false;
- //   exist_ed=prime_fac(X2,X3,X5,surpus_D);
- //   if(not_exist_ed){
- //       cout << "サイコロの目ではDの倍数になりません" << "\n" << flush;
- //       cout << 0.0 << "\n" << flush;
- //       return 0;
-//    }
-//    cout << "動的計画法で求めます" << "\n" << flush;
+    can_xi=prime_fac(X2,X3,X5,surpus_D);
+    cout << "D = " << D << " :2^ " << X2 << ": 3^ " << X3 << ": 5^ " << X5
+    << ": D残骸 " << surpus_D << "\n" << flush; 
+    /**
+    if(exist_ed){
+        cout << "サイコロの目ではDの倍数になりません" << "\n" << flush;
+        cout << 0.0 << "\n" << flush;
+        return 0;
+    }
+    cout << "動的計画法で求めます" << "\n" << flush;
+    **/
     return 0;
 }
