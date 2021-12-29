c01a4f96f6c52d17afcc2ae8292d126996cec70f
happyhappyhappyhappy <ymnkkj@gmail.com>
2021-12-29 18:32:21 +0900

    解説までつけた

--- comp_pro/cf/1612A/first/submit1.cpp
+++ comp_pro/cf/1612A/first/submit1.cpp
@@ -9,36 +9,47 @@ void initial(void){
     cout.tie(nullptr);
 }
 
-bool solver(int bx,int by,int& cx,int& cy){
+int solver(int bx,int by,int& cx,int& cy){
+    int result=0;
+    int answer=0;
     if((bx+by)%2==1){
         cx = -1;
         cy = -1;
-        return false;
+        result = -1;
+        return result;
     }
     int i_ax=0;
     int i_ay=0;
     for(int i_cx=0;i_cx<=50;i_cx++){
         for(int i_cy=0;i_cy<=50;i_cy++){
             int a2c=abs(i_cx)+abs(i_cy);
-            int b2c=abs(i_cx-i_bx)+abs(i_cy-i_by);
+            int b2c=abs(i_cx-bx)+abs(i_cy-by);
             if(a2c == b2c){
                 cx = i_cx;
+                cy = i_cy;
+                answer = a2c;
             }
-
         }
     }
-    return true;
+    return answer;
 }
 
 int main(void){
     initial();
     int t_c;
-    cin >> t_c;
+//    cin >> t_c;
+    t_c = 1;
     for(int ii =0;ii<t_c;ii++){
         int b_x,b_y;
         int c_x,c_y;
-        solver(b_x,b_y,c_x,c_y);
-        cout << c_x << " " << c_y;
+        int answer=0;
+        cin >> b_x >> b_y;
+        answer = solver(b_x,b_y,c_x,c_y);
+        if(answer==-1){
+            cout << "-1 -1" << "\n" << flush;
+            return 0;
+        }
+        cout << c_x << " " << c_y << "\n" << flush;
     }
     return 0;
 }
