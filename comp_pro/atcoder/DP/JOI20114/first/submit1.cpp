5a18fa72223a1c9e4c1aa6a3086c6f590cc57d42
happyhappyhappyhappy <ymnkkj@gmail.com>
2022-01-06 15:08:03 +0900

    一旦休憩 2022/01/06 15:08

--- comp_pro/atcoder/DP/JOI20114/first/submit1.cpp
+++ comp_pro/atcoder/DP/JOI20114/first/submit1.cpp
@@ -3,10 +3,42 @@ using namespace std;
 using ll=long long;
 #define COUT(x) cout<<#x<< " = " <<(x)<< " (L" <<__LINE__<< ")" <<"\n" << flush
 
+// 大域変数
+static int MAXN=100;
+static int VMAX=20; // 20220106 この数までJOI君は計算できるものとする
+static int VMIN=0;
+
+ll DP[110][25];
+
 void initial(void){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
+}
+
+void dpclear(void){
+    for(int ii=0;ii<110;ii++){
+        for(int j=0;j<25;j++){
+            DP[ii][j]=0LL;
+        }
+    }
+}
+
+ll solve(){
+    dpclear();
+    int number;
+    cin >> number;
+    ll value[number];
+    for(auto& e:value){
+        cin >> e;
+    }
+    for(int ii=0;ii<number;ii++){
+        cout << value[ii] << " ";
+    }
+    cout << "\n" << flush;
+    ll result=0LL;
+
+    return result;
 }
 
 int main(void){
