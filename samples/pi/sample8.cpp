5da880e0e8761fdd2e6fbbb73a4e9059f2707e4e
happyhappyhappyhappy <yamanaka@gmail.com>
2022-03-05 19:20:18 +0900

    piの検証終わり

--- samples/pi/sample8.cpp
+++ samples/pi/sample8.cpp
@@ -1,5 +1,5 @@
 #include<bits/stdc++.h>
-#include<math>
+#include<math.h>
 using namespace std;
 using ll=long long;
 #define COUT(x) cout<<#x<< " = " <<(x)<< " (L" <<__LINE__<< ")" <<"\n" << flush
@@ -12,12 +12,12 @@ void initial(void){
 
 int main(void){
     initial();
-    int counter=10;
-    int j=counter;
-    //for(int j=1;j<=counter;j=j+1){
-    double answer;
-    answer = circleference(j);
-    cout << answer << "\n" << flush;
-    // }
+    double pi8,pi12;
+    double sqrt2 = sqrt(2.0);
+    double sqrt3 = sqrt(3.0);
+    pi8 = 4*sqrt(2-sqrt2);
+    pi12 = 6*sqrt(2-sqrt3);
+    cout << "Pi for 8 : " << pi8 << "\n" << flush;
+    cout << "Pi for 12 : " << pi12 << "\n" << flush;
     return 0;
 }
