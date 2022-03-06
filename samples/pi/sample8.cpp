787072a44b07b367059784464a5fa64326bcb809
happyhappyhappyhappy <yamanaka@gmail.com>
2022-03-06 14:42:16 +0900

    Πを求めた結果について有効桁数を多くした。

--- samples/pi/sample8.cpp
+++ samples/pi/sample8.cpp
@@ -1,5 +1,5 @@
 #include<bits/stdc++.h>
-#include<math.h>
+#include<cmath>
 using namespace std;
 using ll=long long;
 #define COUT(x) cout<<#x<< " = " <<(x)<< " (L" <<__LINE__<< ")" <<"\n" << flush
@@ -8,6 +8,7 @@ void initial(void){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
+//    std::setprecision(12);
 }
 
 int main(void){
@@ -17,8 +18,8 @@ int main(void){
     double sqrt3 = sqrt(3.0);
     pi8 = 4*sqrt(2-sqrt2);
     pi12 = 6*sqrt(2-sqrt3);
-    cout << "Pi for 8 : " << pi8 << "\n" << flush;
-    cout << "Pi for 12 : " << pi12 << "\n" << flush;
+    cout << "Pi for 8 : " << setprecision(12) <<  pi8 << "\n" << flush;
+    cout << "Pi for 12 : " << pi12 << setprecision(12) << "\n" << flush;
     return 0;
 }
 /*
