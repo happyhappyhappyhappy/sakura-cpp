7a0077ee2f82964eba6e0fc9154d5f3e68360e02
happyhappyhappyhappy <ymnkkj@gmail.com>
2022-05-16 19:13:18 +0900

    NEXTGOできた

--- comp_pro/aoj/ALDS1_4_B/sub/test.cpp
+++ comp_pro/aoj/ALDS1_4_B/sub/test.cpp
@@ -5,20 +5,47 @@ using ll=long long;
 
 struct Node {
     int key;
-    Node* right,left,parent;
+    Node* pright,pleft,pparent;
 };
 
-Node* root,NIL;
+Node* proot,pNIL;
 
 void insert(int k){
-    Node* y = NIL;
-    Node* x = root;
-    Node* z;
-    z = (Node*)malloc(sizeof(Node));
-    z->key = k;
-    z->
+    Node* py = pNIL;
+    Node* px = proot;
+    Node* pz;
+    pz = (Node*)malloc(sizeof(Node));
+    pz->key = k;
+    pz->pleft = pNIL;
+    pz->pright = pNIL;
+
+    while(px != pNIL){
+        py = px;
+        if(pz->key < px->key){
+            px = px->pleft;
+        }
+        else{
+            px = px->pright;
+        }
+    }
+    pz->pparent =py;
+    if(py == pNIL){
+        proot = pz;
+    }
+    else{ // FIXME: P.213 で かつポインタ変数は先頭にpを付けています
+    // NEXTGO: ポインタ変数のpはp_と付けてください。訳分からなくなります。
+        if(pz->key < py->key){
+            y->left = z;
+        }
+        else{
+            y->right =  z;
+        }
+    }
 }
 
+void inorder(Node* u){
+
+}
 void initial(void){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
