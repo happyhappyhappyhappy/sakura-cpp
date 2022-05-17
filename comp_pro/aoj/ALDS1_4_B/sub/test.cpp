01774438678c5cad3128ce3a12f72b90193da94c
happyhappyhappyhappy <ymnkkj@gmail.com>
2022-05-17 18:02:26 +0900

    ひとまずはややこしいポインタ変数を治したとこ

--- comp_pro/aoj/ALDS1_4_B/sub/test.cpp
+++ comp_pro/aoj/ALDS1_4_B/sub/test.cpp
@@ -5,30 +5,30 @@ using ll=long long;
 
 struct Node {
     int key;
-    Node* pright,pleft,pparent;
+    Node* p_right,p_left,p_parent;
 };
 
-Node* proot,pNIL;
+Node* p_root,p_NIL;
 
 void insert(int k){
-    Node* py = pNIL;
-    Node* px = proot;
-    Node* pz;
-    pz = (Node*)malloc(sizeof(Node));
-    pz->key = k;
-    pz->pleft = pNIL;
-    pz->pright = pNIL;
+    Node* p_y = p_NIL;
+    Node* p_x = p_root;
+    Node* p_z;
+    p_z = (Node*)malloc(sizeof(Node));
+    p_z->key = k;
+    p_z->pleft = p_NIL;
+    p_z->pright = p_NIL;
 
-    while(px != pNIL){
-        py = px;
-        if(pz->key < px->key){
-            px = px->pleft;
+    while(p_x != p_NIL){
+        p_y = p_x;
+        if(p_z->key < p_x->key){
+            p_x = p_x->p_left;
         }
         else{
-            px = px->pright;
+            p_x = p_x->p_right;
         }
     }
-    pz->pparent =py;
+    p_z->p_parent =p_y;
     if(py == pNIL){
         proot = pz;
     }
