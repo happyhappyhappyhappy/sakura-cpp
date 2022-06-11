164d9b8ce799ee1ea34d26a0c6c7702b42d4516b
happyhappyhappyhappy <ymnkkj@gmail.com>
2022-06-11 19:20:01 +0900

    end

--- samples/lower_bound/basic.cpp
+++ samples/lower_bound/basic.cpp
@@ -2,11 +2,15 @@
 using namespace std;
 using ll=long long;
 #define COUT(x) cout<<#x<< " = " <<(x)<< " (L" <<__LINE__<< ")" <<"\n" << flush
-
-
-
+// C++標準の物を使う場合
+// https://cpprefjp.github.io/reference/algorithm/lower_bound.html
 
 int main(void){
-
+    vector<int> v = {3, 1, 4, 6, 5};
+    int result=-1;
+    sort(v.begin(),v.end());
+    decltype(v)::iterator it = lower_bound(v.begin(),v.end(),4);
+     
+    cout << "4 -> " << result << "\n" << flush;
     return 0;
 }
