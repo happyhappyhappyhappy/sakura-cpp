d813bcd67e4bdec354632da95be763ab3a912467
happyhappyhappyhappy <ymnkkj@gmail.com>
2022-06-17 17:38:28 +0900

    1パターン目は終わり

--- comp_pro/atcoder/joi2009ho_b/another/README.md
+++ comp_pro/atcoder/joi2009ho_b/another/README.md
@@ -1,5 +1,12 @@
-* 様々な情報からこの問題で二分探索を色々実装する
+# 別解
 
-* [実は位置情報が簡単に取れる](https://drken1215.hatenablog.com/entry/2020/12/22/011600)
+様々な情報からこの問題で二分探索を色々実装する
 
-* [汎用化二分探索](https://qiita.com/drken/items/97e37dd6143e33a64c8c)
+* [1.実は位置情報が簡単に取れる](https://drken1215.hatenablog.com/entry/2020/12/22/011600)
+
+* [2.汎用化二分探索](https://qiita.com/drken/items/97e37dd6143e33a64c8c)
+
+## 結果
+
+* [1.のパターン](https://atcoder.jp/contests/joi2009ho/submissions/32516168)
+
--- comp_pro/atcoder/joi2009ho_b/another/submit2.cpp
+++ comp_pro/atcoder/joi2009ho_b/another/submit2.cpp
@@ -9,10 +9,18 @@ void initial(void){
     cout.tie(nullptr);
 }
 
+ll solver(vector<ll> &dist,ll pos){ // 参照渡しをしないとコピーに時間食う
+    ll result=0;
+    ll nearly_pos=lower_bound(dist.begin(),dist.end(),pos) - dist.begin();
+    result = min(abs(dist[nearly_pos]-pos),abs(pos-dist[nearly_pos-1]));
+    return result;
+}
+
 int main(void){
     initial();
     // 一周の長さ,ピザ屋の数(本店含む),顧客の数
     ll d_all,shop_all,cost_all;// d,n,m
+    ll result=0;
     cin >> d_all >> shop_all >> cost_all;
     // COUTION: sortを使うならばvector列を使う
     //          構成数は'()'を使う
@@ -23,10 +31,13 @@ int main(void){
     shop_pos[shop_all-1]=0;// 本店の位置
     shop_pos[shop_all]=d_all;//番兵として「一周後の本店の位置」
     sort(shop_pos.begin(),shop_pos.end());
-    vector<ll> cost_pos(cost_all);
+   vector<ll> cost_pos(cost_all);
     for(ll j=0;j<cost_all;j++){
-        cin >> shop_pos[j];
+        cin >> cost_pos[j];
     }
-    sort(cost_pos.begin(),cost_pos.end());
+    for(ll j=0;j<cost_all;j++){
+        result = result+solver(shop_pos,cost_pos[j]);
+    }
+    cout << result << "\n" << flush;
     return 0;
 }
