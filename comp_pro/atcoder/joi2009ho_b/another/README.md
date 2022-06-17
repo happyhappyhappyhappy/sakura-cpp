b5d4227c9153c84756fc21f13d670f2c05108cbc
happyhappyhappyhappy <ymnkkj@gmail.com>
2022-06-17 14:40:17 +0900

    anotherバージョンを作成

---
+++ comp_pro/atcoder/joi2009ho_b/another/README.md
@@ -0,0 +1,5 @@
+* 様々な情報からこの問題で二分探索を色々実装する
+
+* [実は位置情報が簡単に取れる](https://drken1215.hatenablog.com/entry/2020/12/22/011600)
+
+* [汎用化二分探索](https://qiita.com/drken/items/97e37dd6143e33a64c8c)
---
+++ comp_pro/atcoder/joi2009ho_b/another/submit2.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll=long long;
+#define COUT(x) cout<<#x<< " = " <<(x)<< " (L" <<__LINE__<< ")" <<"\n" << flush
+// 実は条件を満たす配列位置情報が整数値で取れる
+void initial(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+int main(void){
+    initial();
+    
+
+    return 0;
+}
