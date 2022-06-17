bf33842552b03ef8d844426ba50050a2c98c8e33
happyhappyhappyhappy <ymnkkj@gmail.com>
2022-06-17 14:59:39 +0900

    一旦休憩

--- comp_pro/atcoder/joi2009ho_b/another/submit2.cpp
+++ comp_pro/atcoder/joi2009ho_b/another/submit2.cpp
@@ -11,7 +11,22 @@ void initial(void){
 
 int main(void){
     initial();
-    
-
+    // 一周の長さ,ピザ屋の数(本店含む),顧客の数
+    ll d_all,shop_all,cost_all;// d,n,m
+    cin >> d_all >> shop_all >> cost_all;
+    // COUTION: sortを使うならばvector列を使う
+    //          構成数は'()'を使う
+    vector<ll> shop_pos(shop_all+1);
+    for(ll j=0;j<shop_all-1;j++){
+        cin >> shop_pos[j];
+    }
+    shop_pos[shop_all-1]=0;// 本店の位置
+    shop_pos[shop_all]=d_all;//番兵として「一周後の本店の位置」
+    sort(shop_pos.begin(),shop_pos.end());
+    vector<ll> cost_pos(cost_all);
+    for(ll j=0;j<cost_all;j++){
+        cin >> shop_pos[j];
+    }
+    sort(cost_pos.begin(),cost_pos.end());
     return 0;
 }
