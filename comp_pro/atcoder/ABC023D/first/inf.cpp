d9fcdb5c65e6e5888464b8c0b0e1b73cb358fe62
happyhappyhappyhappy <ymnkkj@gmail.com>
2022-07-10 19:19:15 +0900

    シフト演算子の勉強を振り返っておく

---
+++ comp_pro/atcoder/ABC023D/first/inf.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll=long long;
+#define COUT(x) cout<<#x<< " = " <<(x)<< " (L" <<__LINE__<< ")" <<"\n" << flush
+
+int main(void){
+    ll inf32 = 1LL<<32;
+    ll inf50 = 1LL<<50;
+    ll inf60 = 1LL<<60;
+    cout << "32左移動\t" << inf32 << "\n";
+    cout << ""; 
+    return 0;
+}
---
+++ comp_pro/atcoder/ABC023D/first/submit1.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll=long long;
+#define COUT(x) cout<<#x<< " = " <<(x)<< " (L" <<__LINE__<< ")" <<"\n" << flush
+using INF=1LL<<60;
+
+void initial(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+int main(void){
+    initial();
+
+    return 0;
+}
