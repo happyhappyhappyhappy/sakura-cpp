bd6c13cdb0ce8255370ea61745a309aacd6d82f1
happyhappyhappyhappy <ymnkkj@gmail.com>
2022-07-11 19:24:34 +0900

    2022/07/11の分は一旦これで終了

--- comp_pro/atcoder/ABC023D/first/submit1.cpp
+++ comp_pro/atcoder/ABC023D/first/submit1.cpp
@@ -9,6 +9,29 @@ void initial(void){
     cout.tie(nullptr);
 }
 
+long long solver(int &N,vector<long long> &HIGH,vector<long long> &SPEED){
+    long long result=0;
+    long long top_pos=INF;
+    long long low_pos=0;
+    while(abs(top_pos-low_pos)>1){
+        bool ok=true;
+        long long mid=(top_pos+low_pos)/2;
+        vector<long long> TIME(N,0);// midまでの残り時間
+        for(int j=0;j<N;j++){
+            if(mid<HIGH[j]){ // もうmidを超えている場合
+                ok = false; // 初期化0のまま以降の処理は無効
+            }
+            {
+                TIME[j]=(mid-HIGH[j])/SPEED[j];
+            }
+        }
+        sort(TIME.begin(),TIME.end());
+        for(int j=0;j<N;j++){ // 0[秒]からN-1[秒]まで打とうとしてみる
+            if(TIME[j]<j){}// もし、もうリミットを TODO: この時false
+        }
+    }
+    return result;
+}
 int main(void){
     initial();
     int N;
@@ -18,11 +41,6 @@ int main(void){
     for(int j=0;j<N;j++){
         cin >> H[j] >> S[j];
     }
-    long long top=INF;
-    long long down=0;
-    while(abs(top-down)>1){
-        long long mid=(top+down)/2;
-        // TODO: ここから
-    }
+    cout << solver(N,H,S);
     return 0;
 }
