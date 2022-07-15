bf903a59b4364df95370bf7793b83d6ca4f03df2
happyhappyhappyhappy <ymnkkj@gmail.com>
2022-07-15 19:22:27 +0900

    やはり思っていたことが正解だった。

--- comp_pro/cf/1699A/first/submit1.cpp
+++ comp_pro/cf/1699A/first/submit1.cpp
@@ -9,18 +9,31 @@ void initial(void){
     cout.tie(nullptr);
 }
 
+bool solver(ll n,vector<ll> &X){
+    if(n%2==1){
+        return false;
+    }
+    X[0] = n/2;
+    X[1] = 0;
+    X[2] = 0;
+    return true;
+}
 int main(void){ 
     initial();
     int testcase=0;
     cin >> testcase;
-    while(--testcase){
+    for(int j=0;j<testcase;j++){
         vector<ll> X(3,0);
         ll sum;
         bool ok;
         cin >> sum;
         ok = solver(sum,X);
-        if(ok){
-            // cout TODO: ここから
+        if (ok) {
+            cout << X[0] << " " << X[1] << " " 
+            << X[2] << "\n" << flush;
+        }
+        else{
+            cout << "-1\n" << flush; 
         }
     }
     return 0;
