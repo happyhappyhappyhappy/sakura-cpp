863bd66833af2a34c0014afad0e407c265a391cc
happyhappyhappyhappy <ymnkkj@gmail.com>
2022-07-19 19:25:43 +0900

    黄金比探索は難しい

--- comp_pro/atcoder/ARC054B/sample/theme1/sample2.cpp
+++ comp_pro/atcoder/ARC054B/sample/theme1/sample2.cpp
@@ -2,40 +2,47 @@
 using namespace std;
 using ll=long long;
 #define COUT(x) cout<<#x<< " = " <<(x)<< " (L" <<__LINE__<< ")" <<"\n" << flush
+
 long double f(long double x){
     return pow(x,2.0)-6.0*x+10.0;
 }
-// 黄金比
-void compute_cd(double &a,double &b,double &c,double &d){
-    double GOLD = (sqrt(5.0)-1.0)/2.0;
-    // TODO: どうやら全部使うみたいだ
+
+double goldsearch(double left,double right,int count){
+    double x1,x2;
+    double f1,f2;
+    double GOLD=(1.0+sqrt(5.0))/2.0;
+    double result=0;
+    x1 = (right-left)/((GOLD+1)+left);
+    x2 = (right-left)/(GOLD+left);
+    f1=f(x1);
+    f2=f(x2);
+    for(int j=0;j<count;j++){
+       if(f1<f2){
+        right=x2;
+        x2=x1;
+        x1=(right-left)/((GOLD+1)+left);
+        f1=f(x1);
+       } 
+       else{
+        left=x1;
+        x1=x2;
+        x2=(right-left)/(GOLD+left);
+        f2=f(x2);
+       }
+       cout << j << " : " << "left =" << left
+       << " right = " << right << "\n" << flush;
+    }
+    result = (right+left) / 2.0;
+    double last_y=f(result);
+    cout << "result = " << result << 
+    " last " << last_y << "\n" << flush;
+    return (right+left)/2.0;
 }
+
+
 int main(void){
     cout << setprecision(10) << setiosflags(ios::fixed);
-    long double left,right;
-    left=-100.0;
-    right=100.0;
-    for(int j=0;j<150;j++)// これは繰り返し回数
-    {
-        long double t1;
-        long double t2;
-        t1=(2.0*left+right)/3;
-        t2=(left+right*2.0)/3;
-        if(f(t1)>f(t2)){
-            left = t1;
-        }
-        else{
-            right = t2;
-        }
-        cout << j << " term: " << "left : " << left << " right : " << right
-        << "\n" << flush;
-        if(right-left<=0.000000001){
-            break;
-        }
-    }
-    
-    cout << f(left) << "\n" << flush;
-
+    cout << goldsearch(-100.0,100.0,300) << "\n" << flush;
     
     return 0;
 }
