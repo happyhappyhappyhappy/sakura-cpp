d66c3bbef64c6d80f231b97adf15c70a245b0889
happyhappyhappyhappy <ymnkkj@gmail.com>
2022-08-01 18:20:19 +0900

    一応着手

---
+++ comp_pro/cf/1702B/first/submit1.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll=long long;
+#define COUT(x) cout<<#x<< " = " <<(x)<< " (L" <<__LINE__<< ")" <<"\n" << flush
+
+void initial(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+int solver(string &input_str){
+    int days =0;
+    set<char> his_memory;
+    
+}
+
+int main(void){
+    initial();
+    int testcase=0;
+    cin >> testcase;
+    while(testcase--){
+        string words;
+        cin >> words;
+        cout << words << "\n" << flush;
+    }
+    return 0;
+}
