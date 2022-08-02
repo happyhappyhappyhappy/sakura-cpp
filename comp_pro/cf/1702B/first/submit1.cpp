14e71558e9f8e223c86a69dab1e3e561ec1759fa
happyhappyhappyhappy <ymnkkj@gmail.com>
2022-08-02 19:35:09 +0900

    1702B完了 🤪

--- comp_pro/cf/1702B/first/submit1.cpp
+++ comp_pro/cf/1702B/first/submit1.cpp
@@ -10,9 +10,21 @@ void initial(void){
 }
 
 int solver(string &input_str){
-    int days =0;
+    int days = 1;
     set<char> his_memory;
-    
+    for(int j=0;j<input_str.size();j=j+1){
+        char getHaveChar=input_str.at(j);
+        const int HISCANCHARS = 3;
+        his_memory.insert(getHaveChar);
+        // cout << getHaveChar << " insert\n" << flush; 
+        if(HISCANCHARS < his_memory.size()){// もう彼が覚えられなくなったら
+            his_memory.clear();
+            days=days+1; // 次の日もう一回入れる
+        //    cout << days << " day...input " << "\n" << flush;
+            his_memory.insert(getHaveChar);
+        }
+    }
+    return days;
 }
 
 int main(void){
@@ -22,7 +34,7 @@ int main(void){
     while(testcase--){
         string words;
         cin >> words;
-        cout << words << "\n" << flush;
+        cout << solver(words) << "\n" << flush;
     }
     return 0;
 }
