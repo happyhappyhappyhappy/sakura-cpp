8cfca08a7d8061aa49cb1d02677774dd8262fa5d
happyhappyhappyhappy <ymnkkj@gmail.com>
2022-08-16 19:29:40 +0900

    途中

--- comp_pro/atcoder/ABC023D/second/submit1.cpp
+++ comp_pro/atcoder/ABC023D/second/submit1.cpp
@@ -3,6 +3,7 @@ using namespace std;
 using ll=long long;
 #define COUT(x) cout<<#x<< " = " <<(x)<< " (L" <<__LINE__<< ")" <<"\n" << flush
 #define debug(...) fprintf(stderr,__VA_ARGS__)
+const long long MAX_HIGH=1LLe15;
 
 void initial(void){
     ios_base::sync_with_stdio(false);
@@ -12,6 +13,12 @@ void initial(void){
 
 int main(void){
     initial();
-    
+    int N;
+    vector<ll> H(N);
+    vector<ll> S(N);
+    for(int j=0;j<N;j=j+1){
+        cin >> H(j) >> S(j);
+    }
+    cout << solver(N,H,S) << "\n" << flush;
     return 0;
 }
