ee0882c0bf084e572498d0ed708d75bb112ccc46
happyhappyhappyhappy <ymnkkj@gmail.com>
2022-09-06 19:32:19 +0900

    まだ上手く働かない。

--- comp_pro/atcoder/ABC122BATCoder/first/submit1.cpp
+++ comp_pro/atcoder/ABC122BATCoder/first/submit1.cpp
@@ -15,18 +15,42 @@ void initial(void){
 int solver(string &inStr){
     int result=0;
     int strlen=inStr.length();
-    for(int ie=0;ie < strlen;ie=ie+1){
+    /** for(int ie=0;ie < strlen;ie=ie+1){
         for(int j=ie;j<strlen;j=j+1){
             // 文字列の調査開始
             bool ok=true;
             for(int k=j;k<strlen;k=k+1){
-                if(STR.find(inStr[k])==string::npos)
-                // TODO: okフラグをfalseにすることから開始
+                if(STR.find(inStr[k])==string::npos){
+                    ok=false;
+                }
+            }
+            if(ok){
+                int strleng=j-ie+1;
+                if(result<strleng)
+                    {
+                        result=strleng;
+                    }
+            }
+        }
+    }**/
+    for(int L=0;L<strlen;L=L+1){
+        for(int R=L;R<N;R=R+1){
+            bool ok=true;
+            for(int x=L;x<R+1;x=x+1){
+                cout << inStr[x];
+                if(STR.find(inStr[x])==string::npos){
+                    ok=false;
+                }
+            }
+            cout << "\n" << flush;
+            if(ok){
+                cout << "全部見つかりました\n" <<  flush;
             }
         }
     }
     return result;
 }
+
 
 int main(void){
     initial();
