f9fd135a96691c7104335c562efd69e89c4fb3cd
happyhappyhappyhappy <ymnkkj@gmail.com>
2022-09-08 19:25:31 +0900

    配列まで作成苦労した

--- comp_pro/atcoder/pken2019Ckaraoke/first/submit1.cpp
+++ comp_pro/atcoder/pken2019Ckaraoke/first/submit1.cpp
@@ -12,5 +12,19 @@ void initial(void){
 
 int main(void){
     initial();
+    ll N,M;
+    cin >> N >> M;
+    vector<vector<ll>> A(N,vector<ll>(M));
+    for(ll STD=0;STD<N;STD=STD+1){
+        for(ll SNG=0;SNG<M;SNG=SNG+1){
+            cin >> A[STD][SNG];
+        }
+    }
+    for(ll STD=0;STD<N;STD=STD+1){
+        for(ll SNG=0;SNG<M;SNG=SNG+1){
+            cout << A[STD][SNG] << " ";
+        }
+        cout << "\n" << flush;
+    }
     return 0;
 }
