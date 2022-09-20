2d97a00f16c63c0008609e037e5e3ee98b9e6a45
happyhappyhappyhappy <ymnkkj@gmail.com>
2022-09-20 19:03:11 +0900

    一応スライド作りと共に開発

--- comp_pro/atcoder/ARC096Ahalfnandhalf/first/submit1.cpp
+++ comp_pro/atcoder/ARC096Ahalfnandhalf/first/submit1.cpp
@@ -13,7 +13,17 @@ void initial(void){
 
 int solver(int A,int B,int AB,int X,int Y){
     int result = 1<<28;
-
+    int surplusX=0;
+    int surplusY=0;
+    for(int j=0;j<=2*pow(10,5);j=j+2){
+        int eachPizza= j / 2;
+        if(eachPizza<X){
+            surplusX=X-eachPizza;
+        }
+        else{
+            surplusX=0;
+        }
+    }
     return result;
 }
 
