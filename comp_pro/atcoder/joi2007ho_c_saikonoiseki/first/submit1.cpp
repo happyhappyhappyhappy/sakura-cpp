ef4cb134b28baea9bcc4eacac8d3e2993ee9e363
happyhappyhappyhappy <ymnkkj@gmail.com>
2022-10-17 19:27:58 +0900

    提出用の物を見直している途中

--- comp_pro/atcoder/joi2007ho_c_saikonoiseki/first/submit1.cpp
+++ comp_pro/atcoder/joi2007ho_c_saikonoiseki/first/submit1.cpp
@@ -21,12 +21,56 @@ void initial(void){
     cin.tie(nullptr);
     cout.tie(nullptr);
 }
+bool exitCheck(P &A,P &B,set<P> HASHIRA){
+    bool result = false;
+    P C,D;
+    ll x1,y1,x2,y2;
+    x1 = A.first;
+    y1 = A.second;
+    x2 = B.first;
+    y2 = B.second;
+    C.first = x2 - (y2-y1);
+    C.second= y2 + (x2-x1);
+    D.first = x1 - ( y2-y1);
+    D.second = y1 + (x2-x1);
+    printf("A:( %d , %d ) \n",A.first,A.second);
+    printf("B:( %d , %d ) \n",B.first,B.second);
+    printf("C.first -> %d \n",C.first);
+    printf("C:( %d , %d )\n",C.first,C.second);
+    printf("D:( %d , %d )\n",D.first,D.second);
+    return result;
+}
+
+int solver(int pt,vector<P> &Point){
+    int result = 0;
+    sort(Point.begin(),Point.end());
+    set<P> HASHIRA;
+    for(int j=0;j<pt;j=j+1){
+        HASHIRA.insert(Point[j]);
+    }
+    for(int j=0;j<pt;j=j+1){
+        for(int k=j+1;k<pt;k=k+1){
+            P A=Point[j];
+            P B=Point[k];
+            if(exitCheck(A,B,HASHIRA)){
+                printf("(%d,%d)-(%d,%d)に対する柱は見つかりました\n",A.first,A.second,B.first,B.second);
+            }
+            else{
+                printf("(%d,%d)-(%d,%d)に対する柱は見つかりませんでした\n",A.first,A.second,B.first,B.second);
+            }
+        }
+    }
+    return result;
+}
 
 int main(void){
     initial();
     int coordpoint; // 座標の数
     cin >> coordpoint;
     vector<P> Point(coordpoint);
-    
+    for(int j=0;j<coordpoint;j=j+1){
+        cin >> Point[j].first >> Point[j].second;
+    }
+    printf("%d\n",solver(coordpoint,Point));
     return 0;
 }
