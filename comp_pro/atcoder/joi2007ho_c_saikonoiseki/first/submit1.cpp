9cb161fa4d6875aa0ad5ef80116cc9703a01c56c
happyhappyhappyhappy <ymnkkj@gmail.com>
2022-10-20 19:33:21 +0900

    TLEが出る。set型は時間が掛かるのか。

--- comp_pro/atcoder/joi2007ho_c_saikonoiseki/first/submit1.cpp
+++ comp_pro/atcoder/joi2007ho_c_saikonoiseki/first/submit1.cpp
@@ -14,7 +14,6 @@ template<class XXX> void chmax(XXX &x,XXX y){
     }
 }
 typedef pair<int,int> P;
-// TODO: 最終チェック。問題サイトから問題を落としてあるのでチェックすること
 
 void initial(void){
     ios_base::sync_with_stdio(false);
@@ -36,8 +35,6 @@ bool exitCheck(P &A,P &B,set<P> HASHIRA){
     decltype(HASHIRA)::iterator it_c=HASHIRA.find(C);
     decltype(HASHIRA)::iterator it_d=HASHIRA.find(D);
     if(it_c != HASHIRA.end() && it_d != HASHIRA.end()){
-        debug("C(%d,%d),D(%d,%d)はHASHIRAに登録されています\n",
-        C.first,C.second,D.first,D.second);
         return true;
     }
     return false;
@@ -54,15 +51,11 @@ int solver(int pt,vector<P> &Point){
         for(int k=j+1;k<pt;k=k+1){
             P A=Point[j];
             P B=Point[k];
+            debug("J=%d,K=%dについて調査中\n",j,k);
             if(exitCheck(A,B,HASHIRA)){
-                debug("(%d,%d)-(%d,%d)に対する柱は見つかりました\n",A.first,A.second,B.first,B.second);
                 int diffab=(A.first-B.first)*(A.first-B.first)+
                 (A.second-B.second)*(A.second-B.second);
-                debug("面積は%dです\n",diffab);
                 chmax(result,diffab);
-            }
-            else{
-                debug("(%d,%d)-(%d,%d)に対する柱は見つかりませんでした\n",A.first,A.second,B.first,B.second);
             }
         }
     }
