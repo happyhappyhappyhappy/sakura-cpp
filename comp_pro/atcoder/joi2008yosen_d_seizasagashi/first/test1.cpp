a34d914fceae8eee02fda3f69aa14ff739a0b76d
happyhappyhappyhappy <ymnkkj@gmail.com>
2022-10-27 17:50:28 +0900

    テストケースの実行

---
+++ comp_pro/atcoder/joi2008yosen_d_seizasagashi/first/test1.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll=long long;
+#define COUT(x) cout<<#x<< " = " <<(x)<< " (L" <<__LINE__<< ")" <<"\n" << flush
+#define debug(...) fprintf(stderr,__VA_ARGS__);
+#define bufcl setvbuf(stdout, (char *)NULL, _IONBF, 0); 
+template<class XXX> void chmin(XXX &x,XXX y){
+    if(x > y){
+        x = y;
+    }
+}
+
+int main(void){
+
+    debug("test\n");
+    bufcl;
+    return 0;
+}
