f88e2caeb0566fa297f7dc0e803b8d5232b12ff7
happyhappyhappyhappy <ymnkkj@gmail.com>
2022-10-30 15:58:47 +0900

    ようやく解けた :grin:

--- comp_pro/atcoder/joi2008yosen_d_seizasagashi/README.md
+++ comp_pro/atcoder/joi2008yosen_d_seizasagashi/README.md
@@ -3,16 +3,18 @@
 ## [問題のURL](https://atcoder.jp/contests/joi2008yo/tasks/joi2008yo_d)
 
 ## [解説のURL](https://www.ioi-jp.org/joi/2007/2008-yo-prob_and_sol/2008-yo-t4/review/2008-yo-t4-review.html)
-    * [参考資料1](https://scrapbox.io/Example0911/JOI_08%E4%BA%88%E9%81%B8_D_%E6%98%9F%E5%BA%A7%E6%8E%A2%E3%81%97(%E9%9B%A3%E6%98%93%E5%BA%A65))
 
+```text
+https://scrapbox.io/Example0911/JOI_08%E4%BA%88%E9%81%B8_D_%E6%98%9F%E5%BA%A7%E6%8E%A2%E3%81%97(%E9%9B%A3%E6%98%93%E5%BA%A65)
+```
 ## [この問題の結果](https://atcoder.jp/contests/joi2008yo/submissions?f.Task=joi2008yo_d&f.LanguageName=&f.Status=AC&f.User=)
 
 ---
 
-ヒント
+* [1回目OK](https://atcoder.jp/contests/joi2008yo/submissions/36089231)
 
 | 回数 | 達成日 | 言語 |
 | --- | ----- | ---- |
-| 1回目 |  |  |
+| 1回目 | 2022年10月30日 | C++ |
 | 2回目 |  |  |
 | 3回目 |  |  |
--- comp_pro/atcoder/joi2008yosen_d_seizasagashi/first/submit1.cpp
+++ comp_pro/atcoder/joi2008yosen_d_seizasagashi/first/submit1.cpp
@@ -9,6 +9,8 @@ template<class XXX> void chmin(XXX &x,XXX y){
     }
 }
 
+typedef pair<int,int> PAIR; // typedef <標準> <新名称>;
+
 void initial(void){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -16,33 +18,40 @@ void initial(void){
 }
 
 
-bool solver(int fig_cnt,int photo_cnt,vector<vector<int>> FIG,
-vector<vector<int>> PHOTO,int &ans_x,int &ans_y){
+bool solver(int fig_cnt,int photo_cnt,vector<PAIR> fig,
+vector<PAIR> photo,int &ans_x,int &ans_y){
     map<pair<int,int>,int> collate_map;
     for(int j=0;j<fig_cnt;j=j+1){
-        for(int k=0;k<photo_cnt;k=K+1){
-            // TODO: ここに星座座標と地図座標との移動長さを
-            // 求めてcollate_mapにcollate_map[{x,y}]++;と入れる
+        for(int k=0;k<photo_cnt;k=k+1){
+            int x_move,y_move;
+            x_move = photo[k].first-fig[j].first;
+            y_move = photo[k].second-fig[j].second;
+            collate_map[{x_move,y_move}]++;
         }
     }
-    ans_x=fig_cnt;
-    ans_y=photo_cnt;
-    return true;
+    for(auto &xy:collate_map){
+        if(xy.second == fig_cnt){
+            ans_x = xy.first.first;
+            ans_y = xy.first.second;
+            return true;
+        }
+    }
+    return false;
 }
 
 int main(void){
     initial();
     int fig_cnt; // 図解上での星座の位置
     cin >> fig_cnt;
-    vector<vector<int>> FIG(fig_cnt,vector<int>(2,0));
+    vector<PAIR> FIG(fig_cnt);
     for(int j=0;j<fig_cnt;j=j+1){
-        cin >> FIG[j][0] >> FIG[j][0];
+        cin >> FIG[j].first >> FIG[j].second;
     } 
     int photo_cnt;
     cin >> photo_cnt; // 実際の星空
-    vector<vector<int>> PHOTO(photo_cnt,vector<int>(2,0));
+    vector<PAIR> PHOTO(photo_cnt);
     for(int j=0;j<photo_cnt;j=j+1){
-        cin >> PHOTO[j][0] >> PHOTO[j][1];
+        cin >> PHOTO[j].first >> PHOTO[j].second;
     }
     int ans_x,ans_y;
     if(solver(fig_cnt,photo_cnt,FIG,PHOTO,ans_x,ans_y)){
