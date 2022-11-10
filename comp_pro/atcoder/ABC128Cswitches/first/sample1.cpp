2d337548b441989f3e26659e7afccaf490300deb
happyhappyhappyhappy <ymnkkj@gmail.com>
2022-11-10 18:25:45 +0900

    少し実行してみただけ

--- comp_pro/atcoder/ABC128Cswitches/first/sample1.cpp
+++ comp_pro/atcoder/ABC128Cswitches/first/sample1.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-using ll=long long;
+// using ll=long long;
 #define COUT(x) cout<<#x<< " = " <<(x)<< " (L" <<__LINE__<< ")" <<"\n" << flush
 #define debug(...) fprintf(stderr,__VA_ARGS__)
 template<class XXX> void chmin(XXX &x,XXX y){
@@ -11,20 +11,66 @@ template<class XXX> void chmin(XXX &x,XXX y){
 // TODO: とりあえず
 // https://atcoder.jp/contests/abc128/submissions/5778021
 // をコピーペーストしてどう入力されるか見ておく
-
+/**
+ #define rep(i,a,b) for(int i=a;i<b;i++)
+#define rrep(i,a,b) for(int i=a;i>=b;i--)
+#define fore(i,a) for(auto &i:a)
+#define all(x) (x).begin(),(x).end()
+*/
 int main(void){
 
     int N,M; // スイッチの数,電球の数
     cin >> N >> M;
-    vector<int> S[10]; // Sはint型のvectorになっている
+    vector<vector<int>> S(10,vector<int>(0)); // Sはint型のvectorになっている
     vector<int> P(10); // 2で割った値がこの値になっていれば光る
-    for(int j=0;j<M;j=j+1) // 電球の分をカウント
-    {
-        for(int k=0;k<N;k=k+1){ 
-
-
+    for(int j=0;j<M;j=j+1) // 電球jはスイッチがONになっていれば光る前提か
+    {   
+        int pat ;
+        cin >> pat;
+        for(int k=0;k<pat;k=k+1){
+            int s;
+            cin >> s;
+            s=s-1; // 0-indexにする
+            S[j].push_back(s); 
+        }
+//        debug("%d の要素の数は %ld です\n",j,S[j].size());
+    }
+    for(int j = 0;j<M;j=j+1){
+        cin >> P[j];
+    }
+    debug("入力した情報を出力します\n");
+    for(int j=0;j<M;j=j+1){
+        debug("電球 %d\n",j);
+        debug("利用するスイッチ\n");
+        for(int k=0;k<S[j].size();k=k+1){
+            debug("%d ",S[j].at(k));
+        }
+        debug("\n");
+        debug("上記のスイッチの内");
+        if(P[j] == 1){
+            debug("奇数個");
+        }
+        else{
+            debug("偶数個");
+        }
+        debug("ONになっていれば付きます\n");
+    }
+    int ans = 0;
+    for(int bit=0;bit < (1<<N);bit=bit+1){
+        int ok=0;
+        #define fore(i,a) for(auto &i:a)
+        for(int jm=0;jm<M;jm=jm+1){;
+            int cnt=0;
+            for(auto s:S[jm]){
+                if((bit & (1<<s))!=0){
+                    cnt = cnt+1; // 付いた
+                }
+            }
+        }
+        if(ok == M){
+            ans = ans+1;
         }
     }
-
+    cout << ans << "\n" << flush;
     return 0;
 }
---
+++ comp_pro/atcoder/ABC128Cswitches/first/sample2.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+#define debug(...) fprintf(stderr,__VA_ARGS__)
+//#pragma GCC optimize ("-O3")
+using namespace std; void _main(); int main() { cin.tie(0); ios::sync_with_stdio(false); _main(); }
+typedef long long ll; const int inf = INT_MAX / 2; const ll infl = 1LL << 60;
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a = b; return 1; } return 0; }
+int N, M;
+vector<int> S[10];
+int P[10];
+//---------------------------------------------------------------------------------------------------
+void _main() {
+	cin >> N >> M;
+	for(int i=0;i<M;i=i+1) {
+		int k; cin >> k;
+		for(int j=0;j<k;j=j+1){
+			int s; 
+            cin >> s; 
+            s=s-1;
+			S[i].push_back(s);
+		}
+	}
+	for(int i=0;i<M;i=i+1)
+    {
+        cin >> P[i];
+    }
+	int ans = 0;
+	for(int msk=0; msk < (1 << N);msk=msk+1) {
+		debug("---フィルターコード %d 検索開始---\n",msk);
+        int ok = 0;
+		for(int m=0;m<M;m=m+1) {
+			int cnt = 0;
+			for(auto &s:S[m])
+                {
+                debug("%d番目の電球,%d番目のスイッチについて\n",m,s);  
+                     if (msk & (1 << s))
+                        { 
+                            debug("フィルター%dの組み合わせは %dで参入\n",msk,s);
+                            cnt=cnt+1;
+                        }
+                    else{
+                        debug("フィルター%dの組み合わせは %dでは\n",msk,s);
+                    }
+                 }
+			if (cnt % 2 == P[m]) {
+                debug("%d番目OK\n",m);
+                ok=ok+1;
+                }
+                else{
+                    debug("%d番目:奇数、偶数が合わずNG\n",m);
+                }
+		}
+        debug("フィルター %d について、点灯した合計 %d / %d \n",msk,ok,M);
+		if (ok == M) 
+        {
+            ans=ans+1;
+        }
+	}
+	cout << ans << endl;
+}
