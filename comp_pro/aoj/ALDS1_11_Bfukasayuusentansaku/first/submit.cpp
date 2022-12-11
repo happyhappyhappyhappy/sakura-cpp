8e07a9e040f58fae0af3099f48d8f3b1d2ec87f2
happyhappyhappyhappy <ymnkkj@gmail.com>
2022-12-11 14:11:00 +0900

    ひとまず打ち込みは完了

--- comp_pro/aoj/ALDS1_11_Bfukasayuusentansaku/first/submit.cpp
+++ comp_pro/aoj/ALDS1_11_Bfukasayuusentansaku/first/submit.cpp
@@ -24,8 +24,51 @@ int next(int u){
     return -1;
 }
 
-// TODO: P.280 L.22
+void dfs_visit(int r){
+    for(int j=0;j<n;j=j+1){
+        nt[j];
+    }
+    stack<int> S;
+    S.push(r);
+    color[r]=GRAY;
+    tt=tt+1
+    d[r] = tt;
+    while(!S.empty()){
+        int u = S.top(); // Sの末尾を読む
+        int v = next(u);
+        if(v != -1){
+            if(color[v] == WHITE){
+                color[v] = GRAY;
+                tt = tt +1;
+                d[v] = tt;
+                S.push(v);
+            }
+            else{
+                // 白でない→すでにチェック済み
+                S.pop();
+                color[u] = BLACK;
+                tt=tt+1;
+                f[u];
+            }
+        }
+    }
+}
 
+void dfs(){
+    for(int j=0;j<n;j=j+1){
+        color[j] = WHITE;
+        nt[j]=0;
+    }
+    tt = 0;
+    for(int u = 0;u<n;u=u+1){
+        if(color[u] == WHITE){
+            dfs_visit(u);
+        }
+    }
+    for(int j=0;j<n;j=j+1){
+        cout << j+1 << " " << d[j] << " " << f[j] << "\n" << flush;
+    }
+}
 
 void initial(void){
     ios_base::sync_with_stdio(false);
@@ -35,6 +78,7 @@ void initial(void){
 
 int main(void){
     initial();
-
+    // TODO: 令和4年12月10日(土)ここまでやった
+    // 螺旋本 P.280 L.67
     return 0;
 }
---
+++ comp_pro/aoj/ALDS1_11_Bfukasayuusentansaku/sample/pptt.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll=long long;
+#define COUT(x) cout<<#x<< " = " <<(x)<< " (L" <<__LINE__<< ")" <<"\n" << flush
+#define debug(...) fprintf(stderr,__VA_ARGS__)
+template<class XXX> void chmin(XXX &x,XXX y){
+    if(x > y){
+        x = y;
+    }
+}
+
+int main(void){
+
+    int a=0;
+    int b=1;
+    a=++b;
+    cout << "a=" << a << " :b=" << b << "\n" << flush;
+    return 0;
+}
