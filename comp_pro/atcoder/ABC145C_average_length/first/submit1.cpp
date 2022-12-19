03454ccd48228027556e875d01503f6d8a5932d3
happyhappyhappyhappy <ymnkkj@gmail.com>
2022-12-19 19:29:32 +0900

    途中まで

--- comp_pro/atcoder/ABC145C_average_length/first/submit1.cpp
+++ comp_pro/atcoder/ABC145C_average_length/first/submit1.cpp
@@ -8,17 +8,36 @@ template<class XXX> void chmin(XXX &x,XXX y){
         x = y;
     }
 }
+const double MAX = (double)1<<28;
+
 
 void initial(void){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
+    cout << setprecision(10) << setiosflags(ios::fixed);
 }
 
 double solver(int &pos,vector<int> &v,
 vector<double> &x,vector<double> &y){
-    
-    return 0.0;
+    double result=MAX;
+    do{
+        double now_sum=0;
+        vector<double> n_x(pos+1);
+        vector<double> n_y(pos+1);
+        for(int j=0;j<pos;j=j+1){
+            n_x[j] = x[v[j]];
+            n_y[j] = y[v[j]];
+        }
+        // 最後は最初の位置に戻る
+        n_x[pos] = x[v[0]];
+        n_y[pos] = y[v[0]];
+        for(int j=0;j<pos;j=j+1){
+            // TODO:: distのコーディングから
+            now_sum = now_sum + dist(n_x[j],n_y[j],n_x[j+1],n_y[j+1]);
+        }
+    }while(next_permutation(v.begin(),v.end()))
+    return result;
 }
 
 int main(void){
@@ -37,7 +56,6 @@ int main(void){
         y[j]=(double)sed;
     }
     double ans = solver(pos,v,x,y);
-    cout << setprecision(10) << setiosflags(ios::fixed);
     cout << ans << endl;
     return 0;
 }
