2de1f619c6caa59b11a381032d590cf16ae4dac9
happyhappyhappyhappy <ymnkkj@gmail.com>
2023-02-07 19:26:32 +0900

    KI 2回目＋色々追加

---
+++ comp_pro/atcoder/ABC138D_KI/second/submit2.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+#ifdef LOCAL
+#include"/code/sakura-cpp/comp_pro/debug.h"
+#include"/code/sakura-cpp/comp_pro/touristdebug.h"
+#else
+#define debug1(...) 42
+#define debug2(...) 42
+#define debug(...) 42
+#define to_string(...) 42
+#define debug_out(...) 42
+#endif
+using namespace std;
+using ll=long long;
+
+void initial(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+int main(void){
+    initial();
+    return 0;
+}
+
