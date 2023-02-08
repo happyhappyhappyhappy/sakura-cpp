521013dd3125657f9d930c2d986f8daea593dc73
happyhappyhappyhappy <ymnkkj@gmail.com>
2023-02-08 19:22:48 +0900

    一部着手

--- comp_pro/atcoder/ABC138D_KI/second/submit2.cpp
+++ comp_pro/atcoder/ABC138D_KI/second/submit2.cpp
@@ -11,6 +11,9 @@
 #endif
 using namespace std;
 using ll=long long;
+using Graph=vector<vector<int>>
+Graph G;
+
 
 void initial(void){
     ios_base::sync_with_stdio(false);
@@ -20,6 +23,16 @@ void initial(void){
 
 int main(void){
     initial();
+    int N,Q;
+    cin >> N >> Q;
+    G.resize(N,vector<int>());
+    for(int j=0;j<N;j=j+1){
+        int point1,point2;
+        cin >> point1 >> point2;
+        point1 = point1 - 1;
+        point2 = point2 - 1;
+        G[point1].push_back(point2);
+        G[point2].push_back(point1);
     return 0;
 }
 
