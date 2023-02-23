d9dc04bc0538b0b0fa7e3a60024e38c9cfa3f572
happyhappyhappyhappy <ymnkkj@gmail.com>
2023-02-23 19:35:30 +0900

    1 3Add files for ALDS1_11_C_habayusentansaku problem, including README and testcase

---
+++ comp_pro/aoj/ALDS1_11_C_habayusentansaku/README.md
@@ -0,0 +1,7 @@
+# \[ALDS1_11_C\]\<幅優先探索\>
+
+## [問題のURL](https://onlinejudge.u-aizu.ac.jp/problems/ALDS1_11_C)
+
+## [解説のURL:これは結果](https://onlinejudge.u-aizu.ac.jp/solutions/problem/ALDS1_11_C)
+
+## [この問題の結果](https://onlinejudge.u-aizu.ac.jp/solutions/problem/ALDS1_11_C)
---
+++ comp_pro/aoj/ALDS1_11_C_habayusentansaku/answer1.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+#ifdef LOCAL
+#include"/code/sakura-cpp/comp_pro/debug.h"
+#include"/code/sakura-cpp/comp_pro/t_debug.h"
+#else
+#define debug1(...) 42
+#define debug2(...) 42
+#define debug(...) 42
+#define to_string(...) 42
+#define debug_out(...) 42
+#endif
+using namespace std;
+using ll=long long;
+
+static const int N = 100;
+static const int INF = (1<<21);
+
+int n,M[N][N];
+int d[N];
+
+void bfs(int s){
+    queue<int> q;
+    q.push(s);
+    // 初期化
+    for(int j=0;j<n;j++){
+        d[j]=INF;
+    }
+    d[s]=0;
+    int u;
+    while(!q.empty()){
+        u = q.front();
+        q.pop();
+        for(int v=0;v<n;v++){
+            if(M[u][v] == 0){
+                continue;
+            }
+            if(d[v]!=INF){
+                continue;
+            }
+        }
+    // TODO: ここから 螺旋本P.285参照
+    }
+}
