d602e350b5c010e08ba4fefdbeea0cd7631cbbb2
happyhappyhappyhappy <ymnkkj@gmail.com>
2023-02-26 17:49:54 +0900

    超入門だけはぐれていた

--- comp_pro/atcoder/habayusen_chounyumon/result1.cpp
+++ comp_pro/atcoder/habayusen_chounyumon/result1.cpp
@@ -18,14 +18,34 @@ void initial(void){
     cout.tie(nullptr);
 }
 
+void showG(vector<vector<int>> &G,int N){
+    for(int j=0;j<N;j=j+1){
+        debug1(" %d :",j);
+        for(int k=0;k<(int)G[j].size();k=k+1){
+            debug1(" %d",G[j][k]);
+        }
+        debug1("\n");
+    }
+}
+
+
 int main(void){
     initial();
     int N,M;
     cin >> N >> M;
-    vector<vector<int>> G // TODO: 悔しいがここから
+    vector<vector<int>> G(N);
     for(int j=0;j<M;j++){
-
+        int first,second;
+        cin >> first >> second;
+        G[first].push_back(second);
+        G[second].push_back(second);
     }
+//    showG(G,N);
+    // 幅優先に使う変数群
+    vector<int> dist(N,-1);
+    queue<int> que;
+    // 初期化 0に今いる
+
     return 0;
 }
 
