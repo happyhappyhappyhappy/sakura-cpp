8d359757044a48d0cf9f5a86cd603e678d0c1c4e
happyhappyhappyhappy <ymnkkj@gmail.com>
2023-02-27 19:25:29 +0900

    Commit message: Add breadth first search for Habayusen Chounyumon task

--- comp_pro/atcoder/habayusen_chounyumon/result1.cpp
+++ comp_pro/atcoder/habayusen_chounyumon/result1.cpp
@@ -40,12 +40,30 @@ int main(void){
         G[first].push_back(second);
         G[second].push_back(second);
     }
-//    showG(G,N);
+    showG(G,N);
     // 幅優先に使う変数群
     vector<int> dist(N,-1);
     queue<int> que;
     // 初期化 0に今いる
-
+    dist[0] = 0;
+    que.push(0);
+    while(que.empty() == false){
+        int v = que.front(); // 先頭を取り出し
+        que.pop(); // 先頭を削除
+        debug(v);
+        for(int nextv: G[v]){ // TODO: forをいつもやるように入れる。nextv=G[v][j]で
+            if(dist[nextv]!=-1){ // 発見済み→次へ行く
+                debug1("nextv %d は 発見済み 深さ %d\n",nextv,dist[nextv]);
+                continue;
+            }
+            dist[nextv]=dist[v]+1;
+            debug(v,nextv,dist[nextv]);
+            que.push(nextv);
+        }
+    }
+    for(int v=0;v<N;v=v+1){
+        cout << v << " : " << dist[v] << "\n" << flush;
+    }
     return 0;
 }
 
