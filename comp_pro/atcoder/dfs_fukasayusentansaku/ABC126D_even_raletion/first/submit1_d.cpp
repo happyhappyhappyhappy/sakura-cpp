9162afcb5b6235123f70a2e075aa3777289194e3
happyhappyhappyhappy <ymnkkj@gmail.com>
2023-03-18 19:20:29 +0900

    Add initializer for Graph G and vector c_code to submit1.cpp

--- comp_pro/atcoder/dfs_fukasayusentansaku/ABC126D_even_raletion/first/submit1_d.cpp
+++ comp_pro/atcoder/dfs_fukasayusentansaku/ABC126D_even_raletion/first/submit1_d.cpp
@@ -12,14 +12,12 @@
 using namespace std;
 using ll=long long;
 using Edge=pair<int,long long>;
-using // TODO: 2023/03/18 ここから開始
 using Graph=vector<vector<Edge>>;
 const double pi = 3.141592653589793238;
 const int yamaMAX_INT = 1 << 29;
 const ll yamaMAX_LL = 1LL << 58;
-
-Graph G;
-vector<int> c_code;
+vector<vector<ll>> G;
+vector<int> dir;
 
 void initial(void){
     ios_base::sync_with_stdio(false);
@@ -29,19 +27,20 @@ void initial(void){
 
 int main(void){
     initial();
-    int N; cin >> N;
-    G.assign(N,vector<Edge>());
-    c_code.assign(N,0);
-    for(int j=0;j<N-1;j++){
+    int N ;
+    cin >> N ;
+    dir.assign(N,0);
+    G.assign(N);
+    for(int j=0;j<N-1;j=j+1){
         int u,v;
-        cin >> u >> v;
         ll w;
-        cin >> w;
-        u=u-1;
-        v=v-1;
-        G[u].push_back(Edge(v,w));
-        G[v].push_back(Edge(u,w));
+        cin >> u >> v >> w;
+        u = u-1;
+        v = v-1;
+        G[u][v] = w;
+        G[v][u] = w;
     }
-
+    dfs(0,-1,0);
     return 0;
 }
+
