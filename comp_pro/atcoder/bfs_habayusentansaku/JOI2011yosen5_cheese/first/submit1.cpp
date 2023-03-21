4e05728a707e6c23b4e1f0617325f0c6dd75c98b
happyhappyhappyhappy <ymnkkj@gmail.com>
2023-03-21 18:57:15 +0900

    テストケース追加など

---
+++ comp_pro/atcoder/bfs_habayusentansaku/JOI2011yosen5_cheese/first/submit1.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#ifdef LOCAL
+#include"/code/sakura-cpp/comp_pro/debug.h"
+#include"/code/sakura-cpp/comp_pro/t_debug.h"
+#else
+#define debug1(...) 42
+#define debug2(...) 42
+#define debug(...) 42
+#define to_string(...) 42
+#define debug_out(...) 42
+#endif
+using namespace std;
+using ll=long long;
+const double pi = 3.141592653589793238;
+const int yamaMAX_INT = 1 << 29;
+const ll yamaMAX_LL = 1LL << 58;
+
