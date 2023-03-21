b2ff727557eb2eab8c4ebe9fccff653b8e4d7ecd
happyhappyhappyhappy <ymnkkj@gmail.com>
2023-03-21 19:07:35 +0900

    Add initialization and main logic to JOI2011yosen5_cheese program

--- comp_pro/atcoder/bfs_habayusentansaku/JOI2011yosen5_cheese/first/submit1.cpp
+++ comp_pro/atcoder/bfs_habayusentansaku/JOI2011yosen5_cheese/first/submit1.cpp
@@ -11,7 +11,35 @@
 #endif
 using namespace std;
 using ll=long long;
+using POS=pair<int,int>
 const double pi = 3.141592653589793238;
 const int yamaMAX_INT = 1 << 29;
 const ll yamaMAX_LL = 1LL << 58;
 
+// 大域変数
+vector<vector<char>> G;
+vector<ll> dir;
+
+void initial(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+int main(void){
+    initial();
+    int H,W,N;
+    cin >> H >> W >> N;
+    G.assign(H+2,vector<char>(W+2,'#'));
+    dir.assign(N+1);
+    for(int j=0;j<H;j=j+1){
+        string tmpstr;
+        for(int k=0;k<W;k=k+1){
+            G[j+1][k+1] = tmpstr[k];
+        }
+    }
+    for(int j=0;j<H+2;j=j+1){
+        for(int k=0;k<W+)
+    }
+    return 0;
+}
