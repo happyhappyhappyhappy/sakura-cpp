ee9f00c9133f1e91c3149a1dc75b0b99607a8071
happyhappyhappyhappy <ymnkkj@gmail.com>
2023-03-23 19:26:57 +0900

    各種変更

--- comp_pro/atcoder/bfs_habayusentansaku/JOI2011yosen5_cheese/first/submit1.cpp
+++ comp_pro/atcoder/bfs_habayusentansaku/JOI2011yosen5_cheese/first/submit1.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 #ifdef LOCAL
-#include"/code/sakura-cpp/comp_pro/debug.h"
-#include"/code/sakura-cpp/comp_pro/t_debug.h"
+#include"/code/sakura-cpp/comp_pro/debug/debug.h"
+#include"/code/sakura-cpp/comp_pro/debug/t_debug.h"
 #else
 #define debug1(...) 42
 #define debug2(...) 42
@@ -11,14 +11,16 @@
 #endif
 using namespace std;
 using ll=long long;
-using POS=pair<int,int>
+using POS=pair<int,int>;
 const double pi = 3.141592653589793238;
 const int yamaMAX_INT = 1 << 29;
 const ll yamaMAX_LL = 1LL << 58;
 
 // 大域変数
-vector<vector<char>> G;
-vector<ll> dir;
+vector<vector<char>> G(10000,vector<char>(10000,'X'));
+vector<vector<int>> dir(10000,vector<int>(10000,yamaMAX_INT));
+vector<POS> fact(10,make_pair(0,0));
+
 
 void initial(void){
     ios_base::sync_with_stdio(false);
@@ -26,20 +28,58 @@ void initial(void){
     cout.tie(nullptr);
 }
 
+void showG(int R,int W){
+    for(int j=0;j<R+2;j=j+1){
+        for(int k=0;k<W+2;k=k+1){
+            debug(" %c",G[j][k]);
+        }
+        debug("\n");
+    }
+    debug("\n");
+}
+
+void showFact(int N){
+    for(int j=1;j<=N;j=j+1){
+        int fromh=fact[j-1].first;
+        int fromw=fact[j-1].second;
+        int toh = fact[j].first;
+        int tow = fact[j].second;
+        debug("%d : (%d,%d)->(%d,%d)\n",j,fromh,fromw,toh,tow);
+    }
+}
+
 int main(void){
     initial();
     int H,W,N;
     cin >> H >> W >> N;
-    G.assign(H+2,vector<char>(W+2,'#'));
-    dir.assign(N+1);
-    for(int j=0;j<H;j=j+1){
-        string tmpstr;
-        for(int k=0;k<W;k=k+1){
-            G[j+1][k+1] = tmpstr[k];
+    for(int j=1;j<=H;j=j+1){
+        for(int k=1;k<=W;k=k+1){
+            cin >> G[j][k];
         }
     }
-    for(int j=0;j<H+2;j=j+1){
-        // TODO: ここから for(int k=0;k<W+)
+    showG(H,W);
+    for(int j=0;j<=H;j=j+1){
+        for(int k=0;k<=W;k=k+1){
+            if(G[j][k]=='S'){
+                fact[0]=make_pair(j,k);
+            }
+        }
     }
+    for(int j=1;j<=N;j=j+1){
+        char nowchar= j+'0';
+ //       debug("今調べようとしているのは %c です\n",nowchar);
+        for(int k=0;k<=H;k=k+1){
+            for(int m=0;m<=W;m=m+1){
+                if(G[k][m] == nowchar){
+//                    debug("%d があるのは (%d,%d)でした\n",j,k,m);
+                    fact[j]=make_pair(k,m);
+
+                }
+            }
+        }
+    }
+    showFact(N);
+    // TODO: 2023年3月23日 後は個々に幅優先探索を行えば良い
     return 0;
+
 }
