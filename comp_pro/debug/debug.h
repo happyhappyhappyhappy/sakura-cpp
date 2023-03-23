5eafd1b55c5636ac10c4b4b62d27952c72ec32a9
happyhappyhappyhappy <ymnkkj@gmail.com>
2023-03-23 19:27:40 +0900

    変更

---
+++ comp_pro/debug/debug.h
@@ -0,0 +1,25 @@
+#ifndef _DEBUG_H_
+#define _DEBUG_H_
+
+#ifdef NDEBUG
+    // NDEBUGが定義されているときは何もしない
+    #define debug2( fmt, ... ) ((void)0)
+    #define debug1( ... ) ((void)0)
+
+#else /* !NDEBUG */
+    // NDEBUGが定義されていないときはprintfデバッグ用関数化
+    #include<stdio.h>
+    #include<bits/stdc++.h>
+    #define debug2( fmt, ... ) \
+        fprintf( stderr, \
+                  "[%s] %s:%u # " fmt "\n", \
+                  __DATE__, __FILE__, \
+                  __LINE__, ##__VA_ARGS__ \
+        ) // debug2懇切丁寧にしたもの
+    #define debug(...) fprintf(stderr,__VA_ARGS__) // debug1従来の形(にしていた)
+                                                   // 20230323 こちらをよく使うので
+                                                    // これをdebugに、tourist版はdebugtに変更
+    #define COUT(x) cout<<#x<< " = " <<(x)<< \
+    " (L" <<__LINE__<< ")" <<"\n" << flush
+#endif /* NDEBUG */
+#endif /* _DEBUG_H_ */
