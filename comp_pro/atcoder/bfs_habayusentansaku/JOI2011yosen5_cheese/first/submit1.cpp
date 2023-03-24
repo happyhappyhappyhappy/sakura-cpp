ed34695220ebb96c046bdaebd36a9ef001fb209e
happyhappyhappyhappy <ymnkkj@gmail.com>
2023-03-24 19:35:01 +0900

    Add constants for MAXSIZE and search directions to support breadth-first search algorithm

--- comp_pro/atcoder/bfs_habayusentansaku/JOI2011yosen5_cheese/first/submit1.cpp
+++ comp_pro/atcoder/bfs_habayusentansaku/JOI2011yosen5_cheese/first/submit1.cpp
@@ -15,12 +15,14 @@ using POS=pair<int,int>;
 const double pi = 3.141592653589793238;
 const int yamaMAX_INT = 1 << 29;
 const ll yamaMAX_LL = 1LL << 58;
+const int MAXSIZE=1010;
 
 // 大域変数
-vector<vector<char>> G(10000,vector<char>(10000,'X'));
-vector<vector<int>> dir(10000,vector<int>(10000,yamaMAX_INT));
+vector<vector<char>> G(MAXSIZE,vector<char>(MAXSIZE,'X'));
+vector<vector<int>> dir(MAXSIZE,vector<int>(MAXSIZE,yamaMAX_INT));
 vector<POS> fact(10,make_pair(0,0));
-
+vector<int> dh={1,0,-1,0};
+vector<int> dw={0,1,0,-1};
 
 void initial(void){
     ios_base::sync_with_stdio(false);
@@ -79,7 +81,41 @@ int main(void){
         }
     }
     showFact(N);
-    // TODO: 2023年3月23日 後は個々に幅優先探索を行えば良い
+    int dist=0;
+    for(int j=0;j<N;j=j+1){
+        int fromh=fact[j].first;
+        int fromw=fact[j].second;
+        int toh = fact[j+1].first;
+        int tow = fact[j+1].second;
+        // dirの再初期化を行う
+        dir.assign(MAXSIZE,vector<int>(MAXSIZE,yamaMAX_INT));
+        queue<POS> que;
+        que.push(make_pair(fromh,fromw));
+        dir[fromh][fromw]=0;
+        while(que.empty()==false){
+            POS nowpos=que.front();
+            int nowh=nowpos.first;
+            int noww=nowpos.second;
+            if(nowh==toh && noww==tow){//ゴールにたどり着いた
+                dist=dist+dir[nowh][noww];
+                break;
+            }
+            for(int j=0;j<4;j=j+1){
+                int nexth=nowh+dh[j];
+                int nextw=noww+dw[j];
+                if(G[nexth][nextw]=='X')
+                {
+                    continue;
+                }
+                if(dir[nexth][nextw]==yamaMAX_INT){
+                    dir[nexth][nextw]=dir[nowh][noww]+1;
+                    que.push(make_pair(nexth,nextw));
+                }
+            }
+            que.pop();
+        }
+    }
+    cout << dist << "\n" << flush;
     return 0;
 
 }
