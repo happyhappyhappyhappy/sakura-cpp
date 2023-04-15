62e865abb7e1dc48929c66f0e187e82856a04146
happyhappyhappyhappy <ymnkkj@gmail.com>
2023-04-15 19:29:52 +0900

    Add a function to generate Lunlun numbers recursively and sort the resulting vector in the main function

--- comp_pro/atcoder/dfs_fukasayusentansaku/ABC161D_LunlunNumber/first/copy1.cpp
+++ comp_pro/atcoder/dfs_fukasayusentansaku/ABC161D_LunlunNumber/first/copy1.cpp
@@ -16,9 +16,22 @@ const int yamaMAX_INT = 1 << 29;
 const ll yamaMAX_LL = 1LL << 58;
 const ll resultMAX=3234566667;
 // 大域変数
-vector<ll> lun(10001);// サンプルではここに全部放り込む
+vector<ll> lun;// サンプルではここに全部放り込む
 int K;
-
+void dfs(ll X){
+    if(X > resultMAX){
+        return ;
+    }
+    lun.push_back(X);
+    ll lastNum=X%10;
+    dfs(X*10+lastNum); // 同じ数はOK
+    if(lastNum !=9){
+        dfs(X*10+(lastNum+1)); // 9でなければ1多くしてもOK
+    }
+    if(lastNum != 0){
+        dfs(X*10+(lastNum-1)); // 0で無ければ1少なくしてOK
+    }
+}
 void initial(void){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -26,13 +39,11 @@ void initial(void){
     for(ll N=1;N<=9;N=N+1){
     dfs(N);
     }
-
-}
-void dfs(ll X){
-
+    sort(lun.begin(),lun.end()); // 整える
 }
 int main(void){
     initial();
     cin >> K;
+    cout << lun[K-1] << "\n" << flush;
     return 0;
 }
