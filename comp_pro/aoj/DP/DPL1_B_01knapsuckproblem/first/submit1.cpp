eda5c33fef25bb58abdff4febf801e7ce1e9cdf0
happyhappyhappyhappy <ymnkkj@gmail.com>
2023-05-02 19:36:06 +0900

    最初の入力を追加

---
+++ comp_pro/aoj/DP/DPL1_B_01knapsuckproblem/first/submit1.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+#ifdef LOCAL
+#include"/wrk/sakura-cpp/comp_pro/debug.h"
+#include"/wrk/sakura-cpp/comp_pro/t_debug.h"
+#else
+#define debug(...) 42
+#define debug2(...) 42
+#define debugt(...) 42
+#define to_string(...) 42
+#define debug_out(...) 42
+#endif
+using namespace std;
+using ll=long long;
+const double pi = 3.141592653589793238;
+const int yamaMAX_INT = 1 << 29;
+const ll yamaMAX_LL = 1LL << 58;
+template<class X> inline bool chmin(X& a,X b){
+    if(b<a){
+        a=b;
+        return true;
+    }
+    return false;
+}
+template<class X> inline bool chmax(X& a,X b){
+    if(a<b){
+        a=b;
+        return true;
+    }
+    return false;
+}
+
+void initial(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+int main(void){
+    initial();
+    int N,W;
+    cin >> N >> W;
+
+    return 0;
+}
