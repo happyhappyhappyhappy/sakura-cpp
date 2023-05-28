5d8b928caf46d8f329cfdb70dca61eb69e0acc45
happyhappyhappyhappy <ymnkkj@gmail.com>
2023-05-28 16:57:44 +0900

    解答完了

--- comp_pro/atcoder/bitzentansaku/ABC190C_Bowls_and_Dishes/first/submit1.cpp
+++ comp_pro/atcoder/bitzentansaku/ABC190C_Bowls_and_Dishes/first/submit1.cpp
@@ -22,14 +22,6 @@ void initial(void){
 }
 int solver(int N,int M,int K,vector<vector<int>> &C,vector<vector<int>> &INB){
     int answer=0;
-    debug("--- 各人がボールを乗せる器の候補 ---\n");
-    for(int j=0;j<K;j=j+1){
-        debug(" %3d -> %d or %d\n",j,INB[j][0],INB[j][1]);
-    }
-    debug("--- 条件一覧 ---\n");
-    for(int j=0;j<M;j=j+1){
-        debug("%d : %3d と %3d両方の皿にボールがのっている\n",j,C[j][0],C[j][1]);
-    }
     for(int selpos=0;selpos<(1<<K);selpos=selpos+1){
         vector<int> bitChain(K,0);
         int ok_count=0;
@@ -39,20 +31,11 @@ int solver(int N,int M,int K,vector<vector<int>> &C,vector<vector<int>> &INB){
                 bitChain[bit]=1;
             }
         }
-        debug(" %d\'s select -> ",selpos);
-        for(int bit=0;bit<K;bit=bit+1){
-            debug(" %d ",INB[bit][bitChain[bit]]);
-        }
-        debug("\n");
         // 皿そのものを用意する
         vector<int> D(N+1,0);
         for(int bit=0;bit<K;bit=bit+1){
             int selectDish = INB[bit][bitChain[bit]];
             D[selectDish]=D[selectDish]+1;
-        }
-        debug("-- 各皿の状況 --\n");
-        for(int d=1;d<N+1;d=d+1){
-            debug(" 皿 %d -> %d 個\n",d,D[d]);
         }
         // 条件確認
         for(int j=0;j<M;j=j+1){
@@ -62,7 +45,6 @@ int solver(int N,int M,int K,vector<vector<int>> &C,vector<vector<int>> &INB){
                 ok_count=ok_count+1;
             }
         }
-        debug("満たす条件 %d 個\n",ok_count);
         if(answer < ok_count){
             answer = ok_count;
         }
