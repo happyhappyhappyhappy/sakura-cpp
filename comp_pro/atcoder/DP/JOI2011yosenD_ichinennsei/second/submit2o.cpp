51df8f957e43d436528577972213296500441d95
happyhappyhappyhappy <ymnkkj@gmail.com>
2023-05-29 19:36:47 +0900

    一年生完了

--- comp_pro/atcoder/DP/JOI2011yosenD_ichinennsei/second/submit2o.cpp
+++ comp_pro/atcoder/DP/JOI2011yosenD_ichinennsei/second/submit2o.cpp
@@ -21,18 +21,45 @@ void initial(void){
     cout.tie(nullptr);
 }
 
+void showValue(vector<vector<ll>> &DP){
+    int H=(int)DP.size();
+    int W=(int)DP[0].size();
+    for(int h=0;h<H;h=h+1){
+        debug(" %3d 個目: ",h+1);
+        for(int w=0;w<W;w=w+1){
+            debug(" %3lld",DP[h][w]);
+        }
+        debug("\n");
+    }
+}
+
+
 int main(void){
     initial();
     int Xall=0;// 取り得る全部の値
     cin >> Xall;
     int X=Xall-1; // 足し引きする合計数
-    vector<int> V(X,0); // 足し引き要素
+    vector<ll> V(X,0); // 足し引き要素
     int R=0; // 確認する結果
     for(int j=0;j<X;j=j+1){
         cin >> V[j];
     }
     cin >> R;
-    for(int j=0;j<X;j=j+1)// 要はこれを得たい組み合わせをアウトプット
-
+    vector<vector<ll>> DP(X,vector<ll>(21,0LL));// 結果には20も入るので
+    DP[0][V[0]]=1LL;
+    for(int j=0;j<X-1;j=j+1){
+       for(int k=0;k<=20;k=k+1){
+            int next_plus=k+V[j+1];
+            int next_minus=k-V[j+1];
+            if(next_plus<=20){
+                DP[j+1][next_plus]=DP[j+1][next_plus]+DP[j][k];
+            }
+            if(0<=next_minus){
+                DP[j+1][next_minus]=DP[j+1][next_minus]+DP[j][k];
+            }
+       }
+    }
+    showValue(DP);
+    cout << DP[X-1][R] << "\n" << flush;
     return 0;
 }
