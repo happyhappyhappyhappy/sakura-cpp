c69bc1252dafcafe8701c63d35f0f3de3aa2e4e0
happyhappyhappyhappy <ymnkkj@gmail.com>
2023-06-15 12:26:42 +0900

    Add input reading for witness counts

--- comp_pro/atcoder/bitzentansaku/ABC147C_HonestOrUnkind2/first/submit1d.cpp
+++ comp_pro/atcoder/bitzentansaku/ABC147C_HonestOrUnkind2/first/submit1d.cpp
@@ -14,7 +14,9 @@ using ll=long long;
 const double pi = 3.141592653589793238;
 const int yamaMAX_INT = 1 << 29;
 const ll yamaMAX_LL = 1LL << 60;
-
+vector<vector<int>> honests;
+vector<vector<int>> liars;
+int N;
 void initial(void){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -22,5 +24,16 @@ void initial(void){
 }
 int main(void){
     initial();
+    cin >> N;
+    for(int j=0;j<N;j=j+1){
+        string liarL;
+        string honestL;
+        int S;// 発言数
+        cin >> S;
+        for(int k=0;k<S;k=k+1){
+            int x=-1;
+            int y=-1;
+        }
+    }
     return 0;
 }
