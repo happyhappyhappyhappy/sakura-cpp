267c70f346b9f410f89994f74dd665744afc12a4
happyhappyhappyhappy <ymnkkj@gmail.com>
2023-06-16 16:01:25 +0900

    Add solution for ABC147C

--- comp_pro/atcoder/bitzentansaku/ABC147C_HonestOrUnkind2/first/submit1d.cpp
+++ comp_pro/atcoder/bitzentansaku/ABC147C_HonestOrUnkind2/first/submit1d.cpp
@@ -19,126 +19,113 @@ template<class XXX> void chmax(XXX &x,XXX y){
         x = y;
     }
 }
-vector<vector<int>> honests;
-vector<vector<int>> liars;
-int N;
+vector<vector<int>> HONEST; // 発言時正直テーブル
+vector<vector<int>> LIAR; // 発言時うそつきテーブル
+int N; // 発言者数
+
 void initial(void){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
 }
-void showHLFigure(void){
+/**
+void showHorL(void){
     for(int j=0;j<N;j=j+1){
-        debug("%3d さんの発言:\n",j);
-        debug("\t正直者 : ");
-        if(honests[j].size()==0){
-            debug("いません");
-        }
-        else{
-            for(int k=0;k<(int)honests[j].size();k=k+1){
-                debug("%4d",honests[j][k]);
+        debug("%3d 氏の発言\n",j);
+        int h_say=(int)HONEST[j].size();
+        int l_say=(int)LIAR[j].size();
+        debug("正直者 : ");
+        if(h_say == 0){
+            debug(" 不在 \n");
+        }else{
+            for(int k=0;k<h_say;k=k+1){
+                debug(" %3d ",HONEST[j][k]);
             }
+            debug("\n");
         }
-        debug("\n");
-        debug("\tうそつき : ");
-        if(liars[j].size()==0){
-            debug("いません");
-        }
-        else{
-            for(int k=0;k<(int)liars[j].size();k=k+1){
-                debug("%4d",liars[j][k]);
+        debug("うそつき : ");
+        if(l_say == 0){
+            debug(" 不在 \n");
+        }else{
+            for(int k=0;k<l_say;k=k+1){
+                debug(" %3d ",LIAR[j][k]);
             }
-        }
-        debug("\n");
-    }
-}
-void showMusk(int &X){
-    debug("------ %4dの状態 ------\n",X);
-    vector<int> honestL(N,-1);
-    vector<int> liarL(N,-1);
-    for(int j=0;j<N;j=j+1){
-        int yorn=(X>>j) & 1;
-        if(yorn==1){
-            honestL[j]=1;
-        }
-        else{
-            liarL[j] = 1;
+            debug("\n");
         }
     }
-    debug("正直者チェック->");
-    for(int j=0;j<N;j=j+1){
-        debug(" %3d",honestL[j]);
-    }
-    debug("\n");
-        debug("うそつきチェック->");
-    for(int j=0;j<N;j=j+1){
-        debug(" %3d",liarL[j]);
-    }
-    debug("\n");
 }
-void showMask2(int &mask){
-    debug("----- ");
-    for(int j=0;j<N;j=j+1){
-        int x=0;
-        x = (mask>>j)&1;
-        debug(" %d",x);
+**/
+/**
+void showBit(int &X){
+    debug("----");
+    for(int S=0;S<N;S=S+1){
+        int F=(X>>S)&1;
+        debug("%3d",F);
     }
-    debug("-----\n");
+    debug("----\n");
 }
+**/
+
 int solver(void){
     int res=0;
-    for(int mask=0;mask<(1<<N);mask=mask+1){
+//    showHorL();
+    for(int B=1;B<(1<<N);B=B+1){ // B:Bit列
         bool ok=true;
         int total=0;
-        showMask2(mask);
-        for(int j=0;j<N;j=j+1){
-            if(((mask>>j) & 1)==1){
-                debug("%3dが正しいと仮定\n",j);
+//        showBit(B);
+        for(int S=0;S<N;S=S+1){ // S:Scale桁確認
+            int H_FL = (B>>S) & 1;// 正直者フラグ
+            if(H_FL==1){
                 total=total+1;
-                for(auto &honest:honests[j]){
-                    if(((mask>>honest)&1)==0){
-                        debug("%3d情報で正直の「マスクと初期データが」矛盾する\n",honest);
+                // 正直者検索
+                for(auto &m:HONEST[S]){
+                    int honeQ=(B >> m) & 1;
+                    if(honeQ==0){
+                        debug("%3dが言う正直者->%3d 今の仮定では正直者ではありません。矛盾\n",S,m);
                         ok=false;
                     }
                 }
-                for(auto &liar:liars[j]){
-                    if(((mask>>liar)&1)==1){
+                // ウソつき検索
+                for(auto &m:LIAR[S]){
+                    int liarQ=(B>>m)&1;
+                    if(liarQ==1){// 実は正直だった
+                        debug("%3dが言う、うそつき->%3d 今の仮定ではうそつきではありません。矛盾\n",S,m);
                         ok=false;
-                        debug("%3d情報でウソつきの「マスクと初期データが」矛盾する\n",liar);
                     }
                 }
             }
         }
-    if(ok==true){
-        debug("%3d -> OK\n",mask);
-        chmax(res,total);
+        if(ok){
+            debug("この仮定においては矛盾していません\n");
+            chmax(res,total);
         }
     }
     return res;
 }
+
 int main(void){
     initial();
+// データ挿入
     cin >> N;
     for(int j=0;j<N;j=j+1){
-        vector<int> liarL;
-        vector<int> honestL;
-        int S;// 発言数
-        cin >> S;
-        for(int k=0;k<S;k=k+1){
-            int x=-1;
-            int y=-1;
-            cin >> x >> y;
-            if(y==1){
-                honestL.push_back(x-1);
+        vector<int> honest_list;
+        vector<int> liar_list;
+        int said;
+        cin >> said;
+        for(int k=0;k<said;k=k+1){
+            int who,cont;
+            cin >> who >> cont;
+            if(cont == 1){
+                honest_list.push_back(who-1); // 0-index
             }
             else{
-                liarL.push_back(x-1);
+                liar_list.push_back(who-1);
             }
         }
-        honests.push_back(honestL);
-        liars.push_back(liarL);
+        debug("%3d のデータ入力\n",j);
+        HONEST.push_back(honest_list);
+        LIAR.push_back(liar_list);
     }
-    showHLFigure();
     cout << solver() << "\n" << flush;
     return 0;
 }
