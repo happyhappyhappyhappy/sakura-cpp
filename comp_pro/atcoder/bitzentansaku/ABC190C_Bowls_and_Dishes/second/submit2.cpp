2b99257831dbc83f4396c4aebbaa545f6b1a9f80
happyhappyhappyhappy <ymnkkj@gmail.com>
2023-06-25 18:11:43 +0900

    Add ABC190C Bowls and Dishes code

--- comp_pro/atcoder/bitzentansaku/ABC190C_Bowls_and_Dishes/second/submit2.cpp
+++ comp_pro/atcoder/bitzentansaku/ABC190C_Bowls_and_Dishes/second/submit2.cpp
@@ -1,14 +1,4 @@
 #include<bits/stdc++.h>
-#ifdef LOCAL
-#include"/wrk/sakura-cpp/comp_pro/debug.h"
-#include"/wrk/sakura-cpp/comp_pro/t_debug.h"
-#else
-#define debug(...) 42
-#define debug2(...) 42
-#define debugt(...) 42
-#define to_string(...) 42
-#define debug_out(...) 42
-#endif
 using namespace std;
 using ll=long long;
 const double pi = 3.141592653589793238;
@@ -36,16 +26,6 @@ void initial(void){
     cin.tie(nullptr);
     cout.tie(nullptr);
 }
-void showData(void){
-    debug("----条件一式----\n");
-    for(int j=0;j<M;j=j+1){
-        debug("%d -> %d and %d\n",j+1,A[j],B[j]);
-    }
-    debug("----投入皿一式(ママ)----\n");
-    for(int j=0;j<K;j=j+1){
-        debug("%d -> %d or %d \n",j+1,C[j],D[j]);
-    }
-}
 string bitPat(int X){
     string ans="";
     for(int j=0;j<K;j=j+1){
@@ -61,7 +41,6 @@ string bitPat(int X){
 }
 int solver(void){
     int ans=0;
-    // showData();
     for(int bit=0;bit<(1<<K);bit=bit+1){
         // K 人ボックス投入
         // 具体的な皿。Dにすると別の大域変数になるのでNG
@@ -69,28 +48,18 @@ int solver(void){
         // 0 番はダミー
         vector<int> BOX(N+1,0);
         string bitp=bitPat(bit);
-        debug("bit パターン : %s\n",bitp.c_str());
         for(int j=0;j<K;j=j+1){
             int selFlug=(bit>>j)&1;
             if(selFlug==1){
-                debug("%d番目の人は Dのさらに入れる\n",j+1);
                 BOX[D[j]]=BOX[D[j]]+1;
             }
             else{
-                debug("%d番目の人は Cのさらに入れる\n",j+1);
                 BOX[C[j]]=BOX[C[j]]+1;
             }
         }
-        debug("%s 投入完了\n",bitp.c_str());
-        debug("中身を見ます\n");
-        for(int j=1;j<=N;j=j+1){
-            debug(" %3d",BOX[j]);
-        }
-        debug("\n");
         int count=0;
         for(int j=0;j<M;j=j+1){
             if(BOX[A[j]]!=0 && BOX[B[j]]!=0){
-                debug("%d 番目の条件が一致しました\n",j+1);
                 count=count+1;
             }
         }
