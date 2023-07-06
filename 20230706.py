df33e9d5aeb6e7999e0aa61c67609047eeb5f78a
happyhappyhappyhappy <ymnkkj@gmail.com>
2023-07-06 19:43:21 +0900

    プログラムを完成＋次回予告を記述

---
+++ 20230706.py
@@ -0,0 +1,24 @@
+from pdfminer.pdfinterp import PDFResourceManager
+from pdfminer.converter import TextConverter
+from pdfminer.pdfinterp import PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
+from pdfminer.layout import LAParams
+from io import StringIO
+
+pdf_file_path = "editorial.pdf"
+
+with open(pdf_file_path , "rb") as pdf_file: #ファイルオブジェクトを受け取り、変数「pdf_file」に代入。
+    output = StringIO() #コンソールに出力されたテキストを取得するため、IOクラス「StringIO」使用
+    resource_manager = PDFResourceManager()
+    laparams = LAParams()#レイアウトの変更がなければデフォルトのままで
+    text_converter = TextConverter(resource_manager, output, laparams=laparams)
+    page_interpreter = PDFPageInterpreter(resource_manager, text_converter)
+
+    for i_page in PDFPage.get_pages(pdf_file): #1ベージずづ処理
+        page_interpreter.process_page(i_page)
+
+    output_text = output.getvalue()
+    output.close()
+    text_converter.close()
+
+print(output_text)
--- comp_pro/atcoder/bitzentansaku/ABC197C_ORXOR/second/submit2.cpp
+++ comp_pro/atcoder/bitzentansaku/ABC197C_ORXOR/second/submit2.cpp
@@ -1,14 +1,4 @@
 #include<bits/stdc++.h>
-#ifdef LOCAL
-#include"/wrk/sakura-cpp/comp_pro/debug.h"
-#include"/wrk/sakura-cpp/comp_pro/t_debug.h"
-#else
-#define debug(...) 42
-#define debug2(...) 42
-#define debugt(...) 42
-#define to_string(...) 42
-#define debug_out(...) 42
-#endif
 using namespace std;
 using ll=long long;
 const double pi = 3.141592653589793238;
@@ -31,30 +21,6 @@ void initial(void){
     cout.tie(nullptr);
     yamaMAX_INT=(1<<30)+1;
 }
-string bit2str(int X,int BASE){
-    string res="";
-    for(int bit=0;bit<BASE;bit=bit+1){
-        int flag=(X >> bit)&1;
-        if(flag==1){
-            res=res+'|';
-        }else{
-            res=res+'-';
-        }
-    }
-    return res;
-}
-void showTable(vector<vector<int>> &X,string &pat){
-    debug("---- 開始 %s の作成結果 ----\n",pat.c_str());
-    int S1=X.size(); // XORを作成する回数
-    for(int j=0;j<S1;j=j+1){
-        int S2=X[j].size();
-        for(int k=0;k<S2;k=k+1){
-            debug(" %3d ",X[j][k]);
-        }
-        debug("\n");
-    }
-    debug("----ここまで %s ----\n",pat.c_str());
-}
 int solver(void){
     int result=yamaMAX_INT;
     int fwc=N-1;
@@ -62,7 +28,6 @@ int solver(void){
         string fw;
         vector<vector<int>> XORTable;
         vector<int> ORList;
-        fw = bit2str(bit,fwc);
         ORList.push_back(A[0]);
         for(int j=0;j<fwc;j=j+1){
             int flag= (bit>>j) & 1;
@@ -94,8 +59,6 @@ int solver(void){
         for(int j=1;j<(int)XORList.size();j=j+1){
             XORTRes=XORTRes ^ XORList[j];
         }
-        showTable(XORTable,fw);
-        debug("この時の計算結果=> %d\n",XORTRes);
         chmin(result,XORTRes);
         XORTable.clear();
     }
