49bd84eb4657ceb4bf40ccdd369d88bedde8a153
happyhappyhappyhappy <ymnkkj@gmail.com>
2023-07-08 19:31:53 +0900

    何とか作れそうなので続ける

---
+++ comp_pro/atcoder/dfs_fukasayusentansaku/ABC114C753suu/testd.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+#ifdef LOCAL
+#include<debug.h>
+#include<t_debug.h>
+#else
+#define debug(...) 42
+#define debug2(...) 42
+#define debugt(...) 42
+#define to_string(...) 42
+#define debug_out(...) 42
+#endif
+using namespace std;
+using ll=long long;
+using num753=tuple<ll,bool,bool,bool>;
+const double pi = 3.141592653589793238;
+const int yamaMAX_INT = 1 << 29;
+const ll yamaMAX_LL = 1LL << 58;
+
+void initial(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+ll solver(ll N){
+    ll answer=0;
+    num753 first=make_tuple(0,false,false,false);
+    stack<num753> S;
+    S.push(first);
+    while(S.empty()==false){
+        num753 nowSet=S.top();
+        S.pop();
+        ll num=get<0>(nowSet);
+        bool has3=get<1>(nowSet);
+        bool has5=get<2>(nowSet);
+        bool has7=get<3>(nowSet);
+        if(N < num){
+            continue;
+        }
+        if ((has3 && has5 && has7)==true){
+            debugt(num);
+            answer = answer+1;
+        }
+        vector<num753> T(3);
+        T[0]=make_tuple(num*10+3,true,has5,has7);
+        T[1]=make_tuple(num*10+5,has3,true,has7);
+        T[2]=make_tuple(num*10+7,has3,has5,true);
+        for(int j=0;j<3;j=j+1){
+            S.push(T[j]);
+        }
+    }
+
+
+    return answer;
+}
+
+int main(void){
+    initial();
+    ll N;
+    cin >> N;
+    cout << solver(N) << "\n" << flush;
+    return 0;
+}
+
---
+++ comp_pro/atcoder/dfs_fukasayusentansaku/ABC119C_Synthetic_Kadomatsu/first/submit1d.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+#ifdef LOCAL
+#include<debug.h>
+#include<t_debug.h>
+#else
+#define debug(...) 42
+#define debug2(...) 42
+#define debugt(...) 42
+#define to_string(...) 42
+#define debug_out(...) 42
+#endif
+using namespace std;
+using ll=long long;
+const double pi = 3.141592653589793238;
+const int yamaMAX_INT = 1 << 29;
+const ll yamaMAX_LL = 1LL << 60;
+// ABC119C Synthetic Kadomatu 再帰関数利用編
+// グローバル変数
+int N; // 材料の竹の数
+int A,B,C; // 完成させたい竹の長さ
+vector<int> L; // 材料の竹の長さ
+
+void showData(void){
+    debug("竹の本数 : %d , 必要な竹の長さ %d , %d , %d \n",N,A,B,C);
+    debug("利用する竹 ");
+    for(auto &e: L){
+        debug(" %d ",e);
+    }
+    debug("\n");
+}
+int dfs(int n,int a,int b,int c){
+    int nowans=0;
+    if(n==N){
+        debug("すべての竹を利用したので増加魔法＋減少魔法を計算します\n");
+        if(min(a,b,c)>0){
+            debug("a = %d , b = %d , c = %d\n",a,b,c);
+            debug("内容はともかく計算できそうなので求めます\n")
+        }else{
+// TODO: 2023-07-08 19:31:23 n==Nのロジックをまず書きましょう
+        }
+    }
+    return nowans;
+}
+int solver(void){
+    int result=0;
+    showData();
+    result = dfs(0,0,0,0);
+    return result;
+}
+void initial(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+int main(void){
+    initial();
+    cin >> N;
+    cin >> A >> B >> C;
+    L.resize(N);
+    for(int j=0;j<N;j=j+1){
+        cin >> L[j];
+    }
+    cout << solver() << "\n" << flush;
+    return 0;
+}
