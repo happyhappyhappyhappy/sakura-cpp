d91fa1adc82124e3e743a4daeadb6d2a8aaf8434
happyhappyhappyhappy <ymnkkj@gmail.com>
2023-07-09 17:56:17 +0900

    迷ったあげくpythonに逃げる2023-07-09 17:56:35

---
+++ comp_pro/atcoder/dfs_fukasayusentansaku/ABC165C_ManyRequirement/first/submit11.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+#ifdef LOCAL
+#include"/wrk/sakura-cpp/comp_pro/debug.h"
+#include"/wrk/sakura-cpp/comp_pro/t_debug.h"
+#else
+#define debug(...) 42
+#define debug2(...) 42
+#define debugt(...) 42
+#define to_string(...) 42
+#define debug_out(...) 42
+#endif
+using namespace std;
+using ll=long long;
+const double pi = 3.141592653589793238;
+const int yamaMAX_INT = 1 << 29;
+const ll yamaMAX_LL = 1LL << 60;
+template<class XXX> void chmax(XXX &x,XXX y){
+    if(x < y){
+        x = y;
+    }
+}
+// グローバル変数
+vector<int> a; // ここから
+vector<int> b; // NOTE:PENDING:2023-07-09 17:55:44 やはりpythonでやろう
+vector<int> c;
+vector<int> d;
+void initial(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+int main(void){
+    initial();
+    return 0;
+}
