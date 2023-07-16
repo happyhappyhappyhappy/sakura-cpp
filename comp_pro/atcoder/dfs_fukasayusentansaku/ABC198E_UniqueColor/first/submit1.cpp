a4cd5885edb596a74ad36ca7329d87a9707e72b1
happyhappyhappyhappy <ymnkkj@gmail.com>
2023-07-16 14:33:07 +0900

    Add dfs function and initial function to solve a problem of UniqueColor

--- comp_pro/atcoder/dfs_fukasayusentansaku/ABC198E_UniqueColor/first/submit1.cpp
+++ comp_pro/atcoder/dfs_fukasayusentansaku/ABC198E_UniqueColor/first/submit1.cpp
@@ -1,14 +1,4 @@
 #include<bits/stdc++.h>
-#ifdef LOCAL
-#include"/wrk/sakura-cpp/comp_pro/debug.h"
-#include"/wrk/sakura-cpp/comp_pro/t_debug.h"
-#else
-#define debug(...) 42
-#define debug2(...) 42
-#define debugt(...) 42
-#define to_string(...) 42
-#define debug_out(...) 42
-#endif
 
 using namespace std;
 using ll=long long;
@@ -56,38 +46,11 @@ void initial(void){
     ANS.assign(N+1,1);
     ANS[0]=0;
 }
-void showAllData(void){
-    debug("\n--- 回答 OK か ダメか ---\n");
-    for(auto &e:ANS){
-        debug("%3d",e);
-    }
-    debug("\n--- 到着したか否か ---\n");
-    for(auto &e:R){
-        debug("%3d",e);
-    }
-    debug("\n--- 今までこの色が使われた回数 ---\n");
-    for(auto &e:DC){
-        debug("%3d",e);
-    }
-    debug("\n--- 各頂点に対する 辺---\n");
-    for(int j=1;j<int(G.size());j=j+1){
-        debug("%3d :",j);
-        for(auto &e:G[j]){
-            debug("%3d ",e);
-        }
-        debug("\n");
-    }
-    debug("\n 頂点毎の色 \n");
-    for(auto &e:C){
-        debug("%3d",e);
-    }
-    debug("\n");
-}
+
 void dfs(int t){
     int color_no=C[t];
     int color_cnt=DC[color_no];
     if(color_cnt != 0){
-        debug("頂点 %d Warning ! 色 %d はもう置いてある\n",t,color_no);
         ANS[t]=0; // もうすでに色を使っていたら
     }
     DC[color_no]=DC[color_no]+1;
@@ -96,18 +59,13 @@ void dfs(int t){
     for(int j=0;j<int(G[t].size());j=j+1){
         int next_t=G[t][j];
         if(R[next_t]==0){
-            debug("頂点 %d に進出\n",next_t);
             dfs(next_t);
-        }
-        else{
-            debug("頂点 %d は以前に通ったので通過\n",next_t);
         }
     }
     DC[color_no]=DC[color_no]-1;
 }
 int main(void){
     initial();
-    showAllData();
     dfs(1); // まずは提案通り最初の頂点だけ流す
     for(int j=0;j<N+1;j=j+1){
         if(ANS[j]==1){
