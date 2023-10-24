64ba1b8b7227949380ad515cdee1c81750ca3b3a
happyhappyhappyhappy <ymnkkj@gmail.com>
2023-10-24 19:31:43 +0900

    lazysegment 感覚を付けたい

---
+++ comp_pro/atcoder/lazysegment/AOJ_RMQ_AND_RAQ/aclcopy.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+#include<atcoder/all>
+#ifdef LOCAL
+#include"/wrk/sakura-cpp/comp_pro/debug.h"
+#include"/wrk/sakura-cpp/comp_pro/t_debug.h"
+#include"/wrk/sakura-cpp/lib/dump.hpp"
+#else
+#define debug(...) 42
+#define debug2(...) 42
+#define debugt(...) 42
+#define to_string(...) 42
+#define debug_out(...) 42
+#endif
+using namespace std;
+using ll=long long;
+const double pi = 3.141592653589793238;
+const int yamaMAX_INT = 1 << 29;
+const ll yamaMAX_LL = 1LL << 60;
+using S = long long;
+using F = long long;
+const S INF = 8e18;
+S op(S a,S b){
+    return std::min(a,b);
+}
+S e(){
+    return INF;
+}
+// TODO: ここからacl使うよーと言う感じで作成する
+
+void initial(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+int main(void){
+    initial();
+
+    return 0;
+}
