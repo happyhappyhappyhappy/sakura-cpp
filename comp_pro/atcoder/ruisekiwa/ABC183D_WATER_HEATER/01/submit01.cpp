69498de6ff1623527da422c5ad9831807dacd375
happyhappyhappyhappy <ymnkkj@gmail.com>
2024-01-09 10:37:20 +0000

    ABC183D完了

--- comp_pro/atcoder/ruisekiwa/ABC183D_WATER_HEATER/01/submit01.cpp
+++ comp_pro/atcoder/ruisekiwa/ABC183D_WATER_HEATER/01/submit01.cpp
@@ -1,4 +1,7 @@
 #include<bits/stdc++.h>
+// WAと推定。累積和の0以上制限を消してみたとき
+// 通りました。0以下にはならないということか。
+// https://atcoder.jp/contests/abc183/submissions/49182119
 #ifdef LOCAL
 #include"/wrk/sakura-cpp/comp_pro/debug.h"
 #include"/wrk/sakura-cpp/comp_pro/t_debug.h"
@@ -15,32 +18,45 @@ using ll=long long;
 const double pi = 3.141592653589793238;
 const int yamaMAX_INT = 1 << 29;
 const ll yamaMAX_LL = 1LL << 60;
-
+const int R=200100;
 void initial(void){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
 }
+
 int main(void){
     initial();
     int N,S,T;
-    ll W;
+    long long W;
     cin >> N >> W;
     // 下は200000で固定 人数では無い
-    vector<ll> P(N+1,0); // いもす法データ収集
-    vector<ll> Ac(N+2,0); // 累積和
+    vector<long long> P(R+1,0); // いもす法データ収集
+    vector<long long> Ac(R+2,0); // 累積和
     for(int j=0;j<N;j=j+1){
         ll p;
         cin >> S >> T >> p;
-        P[S]=p;// 開始
-        P[T]=p*(-1) ;// リセット
+        P[S]=P[S]+p;// 開始
+        P[T]=P[T]+p*(-1) ;// リセット
     }
-    for(int j=0;j<N;j=j+1){
+    for(int j=0;j<R+10;j=j+1){
         Ac[j+1]=Ac[j]+P[j];
+        // if(Ac[j+1]<0){// 0<=Ac[x]となるように調整するこれが勘所か?
+        //     // debug("%d で0以下になったので修正\n",j+1);
+        //     Ac[j+1]=0;
+        // }
     }
-    for(auto& e:Ac){
-        debug("%lld ",e);
+    // for(int j=0;j<20;j=j+1){
+    //     debug("%lld ",Ac[j]);
+    // }
+    // debug("\n");
+    sort(Ac.begin(),Ac.end(),greater<long long>{});
+    // debug("最大値 %lld\n",Ac[0]);
+    // cout << "水の供給量 " << W << ",利用する水の量最大 " << Ac[0] << "\n" << flush;
+    if(W < Ac[0]){
+        cout << "No" << "\n" << flush;
+    }else{
+        cout << "Yes" << "\n" << flush;
     }
-    debug("\n");
     return 0;
 }
